5e8df2075bf697b11165791234559e6978432dfa
Sotos <tsompilis@gmail.com>
2026-02-13 03:13:09 +0200

    configured problem routes

--- clientside/src/App.jsx
+++ clientside/src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Components/Header";
 import ListProblems from "./Components/ListProblems";
 function App() {
   return (
     <>
-      <Header></Header>
-      <ListProblems></ListProblems>
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<ListProblems></ListProblems>}></Route>
+          <Route
+            path="/problems"
+            element={<ListProblems></ListProblems>}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
