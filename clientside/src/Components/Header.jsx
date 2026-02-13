36fc6ac790e0816919f7d64be22ccf86eb0b114b
Sotos <tsompilis@gmail.com>
2026-02-13 02:19:54 +0200

    installed axios

--- clientside/src/Components/Header.jsx
+++ clientside/src/Components/Header.jsx
@@ -2,16 +2,13 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="container">
-      <h3 className="title">All The Methods</h3>
+      <h2 className="title">All The Methods</h2>
       <ul className="links">
         <li className="link">
           <a href="/">Announcements</a>
         </li>
         <li className="link">
           <a href="/">Problems</a>
-        </li>
-        <li className="link">
-          <a href="/">Profile</a>
         </li>
         <li className="link">
           <a href="/">Achievements</a>
