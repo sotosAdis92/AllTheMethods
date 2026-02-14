d282d824cacb9688f65c55c29d53625cf02762fb
Sotos <tsompilis@gmail.com>
2026-02-14 22:33:47 +0200

    handle title

--- clientside/src/Components/ProblemComponent.jsx
+++ clientside/src/Components/ProblemComponent.jsx
@@ -1,17 +1,41 @@
 import { useState } from "react";
 const ProblemComponent = () => {
-  const [number, setNumber] = useState("");
+  const [number, setNumber] = useState(0);
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
   const [difficulty, setDifficulty] = useState("");
   const [description, setDescription] = useState("");
-  const [point, setPoints] = useState("");
+  const [point, setPoints] = useState(0);
+
+  const increment = () => {
+    setNumber(number + 1);
+  };
+  const decrement = () => {
+    setNumber(number - 1);
+  };
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
   return (
     <div className="card">
       <div className="row">
-        <button></button>
-        <button></button>
-        <p></p>
+        <button className="changeNumber" onClick={increment}>
+          +
+        </button>
+        <button className="changeNumber" onClick={decrement}>
+          -
+        </button>
+        <p className="numberText">{number}</p>
+      </div>
+      <div className="row">
+        <input
+          type="text"
+          placeholder="Enter Problem title"
+          name="title"
+          value={title}
+          className="input"
+          onChange={handleTitle}
+        ></input>
       </div>
     </div>
   );
