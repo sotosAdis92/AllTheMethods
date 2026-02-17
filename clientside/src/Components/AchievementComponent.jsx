445008ad51f852074207e415e3859d78cc3496ed
Sotos <tsompilis@gmail.com>
2026-02-17 02:56:34 +0200

    adding card component

--- clientside/src/Components/AchievementComponent.jsx
+++ clientside/src/Components/AchievementComponent.jsx
@@ -1,2 +1,18 @@
-const AchievementComponent = () => {};
+import { useNavigate } from "react-router-dom";
+
+const AchievementComponent = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [rank, setRank] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const navigator = useNavigate;
+  return (
+    <div>
+      <div className="card">
+        <div className="row"></div>
+      </div>
+    </div>
+  );
+};
 export default AchievementComponent;
