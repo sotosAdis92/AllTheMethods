53c5d671abeb08a20255360dd5f557a41c1c04af
Sotos <tsompilis@gmail.com>
2026-02-22 16:32:16 +0200

    finished auth token filter

--- server/allTheMethods/src/main/java/com/example/allTheMethods/jwt/AuthTokenFilter.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/jwt/AuthTokenFilter.java
@@ -43,6 +43,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
         } catch (Exception e){
             logger.error("Cannot set user authentication", e);
         }
+        filterChain.doFilter(request, response);
     }
 
      private String parseJwt(HttpServletRequest request){
