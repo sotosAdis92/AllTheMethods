1d71e1761eaedada9808f7f0bd61bc7b219eb3bc
Sotos <tsompilis@gmail.com>
2026-02-23 02:46:26 +0200

    working auth api

--- server/allTheMethods/src/main/java/com/example/allTheMethods/jwt/JwtUtils.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/jwt/JwtUtils.java
@@ -61,6 +61,7 @@ public class JwtUtils {
         try{
             System.out.println("Validate");
             Jwts.parser().verifyWith((SecretKey) key()).build().parseSignedClaims(authToken);
+            return true;
         } catch (MalformedJwtException e){
             logger.error("Invalid JWT token", e.getMessage());
         } catch (ExpiredJwtException e){
