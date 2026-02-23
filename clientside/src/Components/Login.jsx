7d94c2441997318a44dcf5fc99edc6aad742311b
Sotos <tsompilis@gmail.com>
2026-02-23 20:47:08 +0200

    adding log in form

--- clientside/src/Components/Login.jsx
+++ clientside/src/Components/Login.jsx
@@ -1,7 +1,57 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleInputChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
     <>
-      <h1>Login</h1>
+      <div className="card">
+        <div className="row">
+          <TextField
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            value={formData.username}
+            onChange={handleInputChange}
+          ></TextField>
+        </div>
+        <div className="row">
+          <TextField
+            id="password"
+            label="Password"
+            name="password"
+            autoComplete="current-password"
+            type="password"
+            autoFocus
+            value={formData.password}
+            onChange={handleInputChange}
+          ></TextField>
+        </div>
+        <div className="row">
+          <Button type="submit" variant="contained">
+            Log In
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
