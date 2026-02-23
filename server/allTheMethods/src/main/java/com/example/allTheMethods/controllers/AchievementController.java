19dab9e5f0a6b28c713032e90b40b3c46baf6c2e
Sotos <tsompilis@gmail.com>
2026-02-23 16:44:41 +0200

    changes made to structure, new jwt soon

--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/AchievementController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/AchievementController.java
@@ -5,7 +5,6 @@ import com.example.allTheMethods.dto.ProblemDto;
 import com.example.allTheMethods.service.AchievementService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
