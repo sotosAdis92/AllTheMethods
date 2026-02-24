9bcff73adca30a3112be16b7f9536929848e652f
Sotos <tsompilis@gmail.com>
2026-02-24 20:09:53 +0200

    complete auth controller

--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/AuthController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.example.allTheMethods.dto.AuthenticationResponse;
 import com.example.allTheMethods.dto.SignupRequest;
 import com.example.allTheMethods.dto.UsersDto;
 import com.example.allTheMethods.entity.Users;
+import com.example.allTheMethods.repository.UsersRepository;
 import com.example.allTheMethods.service.AuthService;
 import com.example.allTheMethods.service.UsersService;
 import com.example.allTheMethods.utils.JWTUtil;
@@ -12,13 +13,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -26,11 +33,16 @@ public class AuthController {
     private final AuthService authService;
     private final UsersService usersService;
     private final JWTUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final UsersRepository usersRepository;
 
-    public AuthController(AuthService authService, UsersService usersService, JWTUtil jwtUtil) {
+
+    public AuthController(AuthService authService, UsersService usersService, JWTUtil jwtUtil, AuthenticationManager authenticationManager, UsersRepository usersRepository) {
         this.authService = authService;
         this.usersService = usersService;
         this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
+        this.usersRepository = usersRepository;
     }
 
     @PostMapping("/signup")
@@ -49,6 +61,38 @@ public class AuthController {
             authenticationResponse.setJwtToken(jwt);
             authenticationResponse.setName(createdUser.getDisplayName());
             return ResponseEntity.status(HttpStatus.CREATED).body(authenticationResponse);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("error", "An unexpected error occurred" + e.getMessage()));
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody AuthenticationRequest authenticationRequest){
+        try{
+            authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+            );
+
+            UserDetails userDetails = usersService.userDetailsService().loadUserByUsername(authenticationRequest.getUsername());
+            Optional<Users> optionalUsers = usersRepository.findFirstByUsername(userDetails.getUsername());
+
+            if(optionalUsers.isPresent()){
+                Users users = optionalUsers.get();
+                String jwt = jwtUtil.generateToken(userDetails, users.getUserId());
+                AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+                authenticationResponse.setJwtToken(jwt);
+                authenticationResponse.setName(users.getDisplayName());
+
+                return ResponseEntity.ok(authenticationResponse);
+            }
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "User not found"));
+        } catch (BadCredentialsException e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Collections.singletonMap("message", "Incorrect username or password"));
+        } catch (DisabledException e){
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Collections.singletonMap("message", "User account is disabled"));
+        } catch (UsernameNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonMap("message", "User not found"));
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Collections.singletonMap("error", "An unexpected error occurred" + e.getMessage()));
         }
