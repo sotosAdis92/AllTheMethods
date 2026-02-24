e2e8392985435693ae06b936e2a371d067054080
Sotos <tsompilis@gmail.com>
2026-02-24 20:17:22 +0200

    errors for constructors fixed

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/AuthenticationRequest.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/AuthenticationRequest.java
@@ -6,4 +6,25 @@ import lombok.Data;
 public class AuthenticationRequest {
     private String username;
     private String password;
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
