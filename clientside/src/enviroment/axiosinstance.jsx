67081d2deca581f0e66dd75b053b97e1280ec9de
Sotos <tsompilis@gmail.com>
2026-02-28 00:13:06 +0200

    added axios get for user achievements

--- clientside/src/enviroment/axiosinstance.jsx
+++ clientside/src/enviroment/axiosinstance.jsx
@@ -1,6 +1,20 @@
 import axios from "axios";
+import { getToken } from "./common";
 const instance = axios.create({
   baseURL: "http://localhost:8080/",
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
