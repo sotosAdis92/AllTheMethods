0c75a13b48fc1b3e8060f1b42858f36fc818f124
Sotos <tsompilis@gmail.com>
2026-02-28 13:46:04 +0200

    fixed cors issues

--- server/allTheMethods/src/main/java/com/example/allTheMethods/configs/WebSecurityConfiguration.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/configs/WebSecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.example.allTheMethods.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -38,8 +39,9 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.
                 csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/achievements/**").hasAnyAuthority(UserRole.USER.name())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/user/**").hasAnyAuthority(UserRole.USER.name())
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
