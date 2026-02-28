c2d41aeb6f183bdd177ddebc32ec709fefda270a
Sotos <tsompilis@gmail.com>
2026-02-28 17:58:47 +0200

    added userachievement service get all users achievemnets

--- clientside/src/services/UserAchievementService.js
+++ clientside/src/services/UserAchievementService.js
@@ -1,1 +1,6 @@
-
+import axiosInstance from "../enviroment/axiosinstance";
+const REST_API_BASE_URL = "/api/userAchievements/myAchievements";
+export const getUserAchievements = async () => {
+  const response = await axiosInstance.get(REST_API_BASE_URL);
+  return response;
+};
