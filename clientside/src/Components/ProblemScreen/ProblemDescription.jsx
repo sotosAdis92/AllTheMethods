eb7ad9a9c5e5b25a0504e24609ac31388eeca01f
Sotos <tsompilis@gmail.com>
2026-03-08 03:37:07 +0200

    added react-katex stable version

--- clientside/src/Components/ProblemScreen/ProblemDescription.jsx
+++ clientside/src/Components/ProblemScreen/ProblemDescription.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProblem } from "../../services/ProblemService";
+import {}
 const ProblemDescription = () => {
   const [description, setDescription] = useState("");
   const { id } = useParams();
