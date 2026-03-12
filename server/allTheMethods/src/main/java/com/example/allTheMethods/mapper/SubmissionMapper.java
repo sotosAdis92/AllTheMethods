c2f808ca130baff0585a2a128af0f8c47c4b842e
Sotos <tsompilis@gmail.com>
2026-03-12 03:29:24 +0200

    added correc impl

--- server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/SubmissionMapper.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/SubmissionMapper.java
@@ -1,7 +1,11 @@
 package com.example.allTheMethods.mapper;
 
 import com.example.allTheMethods.dto.SubmissionDto;
+import com.example.allTheMethods.entity.Problem;
 import com.example.allTheMethods.entity.Submission;
+import com.example.allTheMethods.entity.Users;
+import com.example.allTheMethods.repository.ProblemRepository;
+import com.example.allTheMethods.repository.UsersRepository;
 
 public class SubmissionMapper {
     public static SubmissionDto mapToSubmissionDto(Submission submission){
@@ -13,5 +17,14 @@ public class SubmissionMapper {
                 submission.getValid()
         );
     }
-
+    public static Submission mapToSubmission(SubmissionDto submissionDto, UsersRepository usersRepository, ProblemRepository problemRepository){
+        Users user =  usersRepository.findById(submissionDto.getUserId()).orElseThrow();
+        Problem problem = problemRepository.findById(submissionDto.getProblemId()).orElseThrow();
+        Submission submission = new Submission();
+        submission.setDate(submissionDto.getSubmittedAt());
+        submission.setValid(submissionDto.getValid());
+        submission.setUser(user);
+        submission.setProblem(problem);
+        return submission;
+    }
 }
--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceImpl.java
@@ -3,21 +3,30 @@ package com.example.allTheMethods.service.imp;
 import com.example.allTheMethods.dto.SubmissionDto;
 import com.example.allTheMethods.entity.Submission;
 import com.example.allTheMethods.mapper.SubmissionMapper;
+import com.example.allTheMethods.repository.ProblemRepository;
 import com.example.allTheMethods.repository.SubmissionRepository;
+import com.example.allTheMethods.repository.UsersRepository;
 import com.example.allTheMethods.service.SubmmisionService;
 
 public class SubmissionServiceImpl implements SubmmisionService {
     private SubmissionRepository submissionRepository;
+    private UsersRepository usersRepository;
+    private ProblemRepository problemRepository;
 
     public SubmissionServiceImpl(SubmissionRepository submissionRepository) {
         this.submissionRepository = submissionRepository;
     }
 
+    public SubmissionServiceImpl(SubmissionRepository submissionRepository, UsersRepository usersRepository, ProblemRepository problemRepository) {
+        this.submissionRepository = submissionRepository;
+        this.usersRepository = usersRepository;
+        this.problemRepository = problemRepository;
+    }
+
     @Override
     public SubmissionDto createSubmission(SubmissionDto submissionDto) {
-        Submission submission = new Submission();
-        submissionDto = submission.getSubmissionDto();
-        submissionRepository.save(submission);
-        return submissionDto;
+        Submission submission = SubmissionMapper.mapToSubmission(submissionDto, usersRepository, problemRepository);
+        Submission savedSubmission = submissionRepository.save(submission);
+        return SubmissionMapper.mapToSubmissionDto(savedSubmission);
     }
 }
