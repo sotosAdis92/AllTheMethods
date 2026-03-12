1c599447770b1084b86c8f9d703034fea8397f78
Sotos <tsompilis@gmail.com>
2026-03-12 17:41:25 +0200

    added get date

--- clientside/src/Components/ProblemScreen/ProblemDescription.jsx
+++ clientside/src/Components/ProblemScreen/ProblemDescription.jsx
@@ -2,9 +2,13 @@ import "katex/dist/katex.min.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProblem } from "../../services/ProblemService";
+import { getUser } from "../../services/UsersService";
 const ProblemDescription = () => {
   const [description, setDescription] = useState("");
+  const [userId, setUserId] = useState(0);
   const { id } = useParams();
+  const [submissionDate, setSubmissionDate] = useState("");
+
   useEffect(() => {
     getProblem(id)
       .then((response) => {
@@ -16,9 +20,31 @@ const ProblemDescription = () => {
       });
   }, [id]);
 
+  const userIdFunction = () => {
+    getUser()
+      .then((response) => {
+        setUserId(response.data.id);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  useEffect(() => {
+    userIdFunction();
+  }, []);
+
+  const now = new Date();
+  const date = now.getDate();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+
+  const wholeDate = date + "/" + (month + 1) + "/" + year;
+
   const submission = {
     id,
-    description,
+    userId,
+    wholeDate,
   };
   function handleSubmit() {
     console.log(submission);
