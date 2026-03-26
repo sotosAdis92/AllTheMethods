927a3a4267e492581d38bb67e492791bbb802480
Sotos <tsompilis@gmail.com>
2026-03-26 05:06:10 +0200

    adding query for user problems

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/UserProblemService.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/UserProblemService.java
@@ -1,4 +1,9 @@
 package com.example.allTheMethods.service;
 
+import com.example.allTheMethods.dto.UserProblemDto;
+
+import java.util.List;
+
 public interface UserProblemService {
+    List<UserProblemDto> getUserProblems();
 }
--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/UserProblemServiceImlp.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/UserProblemServiceImlp.java
@@ -1,4 +1,33 @@
 package com.example.allTheMethods.service.imp;
 
-public class UserProblemServiceImlp {
+import com.example.allTheMethods.dto.UserProblemDto;
+import com.example.allTheMethods.entity.Users;
+import com.example.allTheMethods.repository.ProblemRepository;
+import com.example.allTheMethods.repository.UserProblemsRepository;
+import com.example.allTheMethods.repository.UsersRepository;
+import com.example.allTheMethods.service.UserProblemService;
+import com.example.allTheMethods.utils.JWTUtil;
+
+import java.util.List;
+
+public class UserProblemServiceImlp implements UserProblemService {
+    private final JWTUtil jwtUtil;
+    private final UserProblemsRepository userProblemsRepository;
+    private final UsersRepository usersRepository;
+    private final ProblemRepository problemRepository;
+
+    public UserProblemServiceImlp(JWTUtil jwtUtil, UserProblemsRepository userProblemsRepository, UsersRepository usersRepository, ProblemRepository problemRepository) {
+        this.jwtUtil = jwtUtil;
+        this.userProblemsRepository = userProblemsRepository;
+        this.usersRepository = usersRepository;
+        this.problemRepository = problemRepository;
+    }
+
+    @Override
+    public List<UserProblemDto> getUserProblems() {
+        Users user = jwtUtil.getLoggedInUser();
+        if(user!=null){
+            List<Object[]> result = userProblemsRepository.findAllByUserId(user.getId());
+        }
+    }
 }
