37c949e3ccc187bdd405ca96959b811d703ff707
Sotos <tsompilis@gmail.com>
2026-03-29 23:47:12 +0300

    fixed repository annotation

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
@@ -3,6 +3,8 @@ package com.example.allTheMethods.repository;
 import com.example.allTheMethods.entity.Problem;
 import com.example.allTheMethods.entity.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubmissionRepository  extends JpaRepository<Submission, Long> {
 }
