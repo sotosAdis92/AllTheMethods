efba73b4598864e5eb3908afeb431cc6243ab254
Sotos <tsompilis@gmail.com>
2026-04-02 04:49:18 +0300

    ast testing

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
@@ -4,14 +4,24 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Iterator;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.tokenizer.*;
-
+import org.springframework.expression.ExpressionParser;
 
 
 public class AbstractTreeBuilder {
     private String f;
     private static Iterator<Token> it = null;
+
     public AbstractTreeBuilder(String f){
         this.f = f;
+        init();
     }
+
+    private void init(){
+        Expression expression = new ExpressionBuilder(f);
+        List <Token> li = Arrays.asList(expression.setVariable());
+    }
+
 }
