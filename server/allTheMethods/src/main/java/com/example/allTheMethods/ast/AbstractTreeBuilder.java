1f6076b7470b0702eb0790b960a86b6114e5224e
Sotos <tsompilis@gmail.com>
2026-04-04 04:44:24 +0300

    working on asr

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
@@ -4,24 +4,29 @@ import java.util.List;
 import java.util.Collections;
 import java.util.Iterator;
 
-import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 import net.objecthunter.exp4j.tokenizer.*;
-import org.springframework.expression.ExpressionParser;
+
 
 
 public class AbstractTreeBuilder {
     private String f;
     private static Iterator<Token> it = null;
+    //Java Iterator Object can be used to loop through collections like ArrayList and HashMaps
+    // it is called Iterator because iterating is looping through a list
+    // to use it, just import it from java.util package
+    // it.next() is the function to loop
+    // Java Token is the smallest meaningful thing to the compiler, they consist of keywords operators constants literals etc
 
     public AbstractTreeBuilder(String f){
+        if(f==null) throw new IllegalArgumentException("Invalid expression");
         this.f = f;
         init();
     }
 
     private void init(){
-        //Expression expression = new ExpressionBuilder(f);
-        //List <Token> li = Arrays.asList(expression.setVariable());
+        ExpressionBuilder expressionBuilder = new ExpressionBuilder(f);
+        List<Token> list = Arrays.asList(expressionBuilder.variable("x").build());
     }
 
 }
