a6c0143e37b5fd44a213b12336e8291352f21a32
Sotos <tsompilis@gmail.com>
2026-04-05 04:08:18 +0300

    getFunction function implimentation testing

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/AbstractTreeBuilder.java
@@ -33,8 +33,8 @@ public class AbstractTreeBuilder {
         it = li.iterator();
     }
 
-    private Operation getFuntion(Token token) throws TokenizerException {
-
+    private Operation getFuntion(Token token){
+        return null;
     }
 
 }
