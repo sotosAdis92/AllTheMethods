1785873fb08bc7b8ed74388b58aba86d2cd7f0ca
Sotos <tsompilis@gmail.com>
2026-04-05 03:59:38 +0300

    tokenizer exception finished

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/TokenizerException.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/TokenizerException.java
@@ -2,7 +2,7 @@ package com.example.allTheMethods.ast;
 
 public class TokenizerException extends ExpressionParser{
     private static final long serialVersionUID = 1L;
-
+    //Class for tokenizer exceptions to be thrown
     public TokenizerException(String message){
         super(message);
     }
