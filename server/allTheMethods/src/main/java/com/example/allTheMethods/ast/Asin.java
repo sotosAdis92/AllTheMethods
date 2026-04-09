6e896a4f941d772019251d603ecd1cd6d08b514e
Sotos <tsompilis@gmail.com>
2026-04-09 05:17:28 +0300

    asin implemented

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Asin.java
@@ -0,0 +1,25 @@
+package com.example.allTheMethods.ast;
+
+public class Asin extends UnaryOperation{
+    public Asin(Operation op){
+        super(op);
+    }
+    public String toString(){
+        return "asin(" + op.toString() + ")";
+    }
+    @Override
+    public Double getNumericResult(Double val){
+        return Math.asin(op.getNumericResult(val));
+    }
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Asin)) return false;
+        Asin asin = (Asin) o;
+        return (op.equals(asin.op));
+    }
+
+    public int hashCode(){
+        return 13 * op.hashCode();
+    }
+}
