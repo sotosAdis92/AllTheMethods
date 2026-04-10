eed758b5d6ee23aa03dc699c06de5552550568f6
Sotos <tsompilis@gmail.com>
2026-04-10 03:54:05 +0300

    division implemented

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Division.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Division.java
@@ -4,5 +4,27 @@ public class Division extends BinaryOperation{
     public Division(Operation left, Operation right){
         super(left, right);
     }
-
+    public Operation getLeft(){
+        return left;
+    }
+    public Operation getRight(){
+        return right;
+    }
+    public String toString(){
+        return "(" + left.toString() + ")/(" + right.toString() + ")";
+    }
+    @Override
+    public Double getNumericResult(Double val){
+        return left.getNumericResult(val) / right.getNumericResult(val);
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(this == obj) return true;
+        if(!(obj instanceof Division)) return false;
+        Division division = (Division) obj;
+        return left.equals(division.left) && right.equals(division.right);
+    }
+    public int hashCode(){
+        return 67 * (left.hashCode() + right.hashCode());
+    }
 }
