95247750a42be32362463a56c920ef8f960f5649
Sotos <tsompilis@gmail.com>
2026-04-10 03:39:01 +0300

    division implemented

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Division.java
@@ -0,0 +1,8 @@
+package com.example.allTheMethods.ast;
+
+public class Division extends BinaryOperation{
+    public Division(Operation left, Operation right){
+        super(left, right);
+    }
+
+}
