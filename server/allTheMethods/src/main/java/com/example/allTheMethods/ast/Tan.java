27cfcad0509fee1eb7d78529195b2fa33ef16003
Sotos <tsompilis@gmail.com>
2026-04-11 04:14:57 +0300

    tan implemented

--- server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Tan.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/ast/Tan.java
@@ -1,4 +1,32 @@
 package com.example.allTheMethods.ast;
 
-public class Tan {
+public class Tan extends UnaryOperation{
+    public Tan(Operation op) {
+        super(op);
+    }
+
+
+    public String toString(){
+        return "tan(" + op.toString() + ")";
+    }
+
+
+    @Override
+    public Double getNumericResult(Double val) {
+        return Math.tan(op.getNumericResult(val));
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (!(o instanceof Tan)) return false;
+        Tan abs = (Tan) o;
+        return (op.equals(abs.op));
+    }
+
+    public int hashCode(){
+        return 59 * op.hashCode();
+    }
+
 }
