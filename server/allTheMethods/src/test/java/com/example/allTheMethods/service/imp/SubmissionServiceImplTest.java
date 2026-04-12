d6684dd868822492115edc25b418eaf2ab225509
Sotos <tsompilis@gmail.com>
2026-04-12 11:59:38 +0300

    First ever unit test for derivative

--- server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceImplTest.java
+++ server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceImplTest.java
@@ -1,13 +1,22 @@
 package com.example.allTheMethods.service.imp;
 
+import com.example.allTheMethods.ast.TokenizerException;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubmissionServiceImplTest {
+
     @Test
     void xSquaredFirstSimpleDerivativeTest(){
         SubmissionServiceImpl ssi = new SubmissionServiceImpl();
+        try{
+            assertEquals(2,ssi.fprime(2,"x^2"));
+        } catch (TokenizerException tokenizerException){
+            System.out.println("Token Exception");
+        }
 
     }
 
