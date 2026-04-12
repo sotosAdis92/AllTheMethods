d06570731f8f4e52375510d04bc1ff3c15d5bab8
Sotos <tsompilis@gmail.com>
2026-04-12 20:58:24 +0300

    installed junit5

--- server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceImplTest.java
+++ server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceImplTest.java
@@ -3,14 +3,16 @@ package com.example.allTheMethods.service.imp;
 import com.example.allTheMethods.ast.TokenizerException;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubmissionServiceImplTest {
-
+    //@Test Annotation kanei thn methodo test methodo
+    //To Framework ths Junit automatos trexei ola ta @Test methods
+    //Xreiazetai to import epano apo to jupiter api
+    //Ta @Test methods den xreiazontai access identifier, mporoun na einai sketa, public, private h protected
     @Test
-    void xSquaredFirstSimpleDerivativeTest(){
+    public void xSquaredFirstSimpleDerivativeTest(){
         SubmissionServiceImpl ssi = new SubmissionServiceImpl();
         try{
             assertEquals(4,ssi.fprime(2,"x^2"));
@@ -20,7 +22,7 @@ class SubmissionServiceImplTest {
     }
 
     @Test
-    void firstDegreePolynomialDerivativeTest(){
+    public void firstDegreePolynomialDerivativeTest(){
         SubmissionServiceImpl ssi = new SubmissionServiceImpl();
         try{
             assertEquals(1, ssi.fprime(4,"2x-x"));
