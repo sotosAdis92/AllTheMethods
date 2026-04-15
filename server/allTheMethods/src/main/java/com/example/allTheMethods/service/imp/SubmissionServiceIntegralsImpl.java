176d69abc2a908ca34a19dd7c287fab98e2aea3f
Sotos <tsompilis@gmail.com>
2026-04-15 03:03:51 +0300

    trapezodial rule class

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
@@ -1,7 +1,22 @@
 package com.example.allTheMethods.service.imp;
 
+import com.example.allTheMethods.service.SubmissionServiceIntegrals;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SubmissionServiceIntegralsImpl {
+public class SubmissionServiceIntegralsImpl implements SubmissionServiceIntegrals {
+    @Override
+    public boolean checkTrapezodialRuleData() {
+        return false;
+    }
+
+    @Override
+    public boolean checkSimpsonData() {
+        return false;
+    }
+
+    @Override
+    public boolean checkRombergData() {
+        return false;
+    }
 }
