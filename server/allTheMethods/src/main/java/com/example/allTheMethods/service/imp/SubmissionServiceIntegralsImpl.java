50b749464213c58dbb9a78409494779501e7fae1
Sotos <tsompilis@gmail.com>
2026-04-15 14:39:58 +0300

    trapezodial rule testing

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
@@ -4,11 +4,25 @@ import com.example.allTheMethods.dto.TrapezodialRuleDataDto;
 import com.example.allTheMethods.service.SubmissionServiceIntegrals;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class SubmissionServiceIntegralsImpl implements SubmissionServiceIntegrals {
     @Override
     public boolean checkTrapezodialRuleData(TrapezodialRuleDataDto trapezodialRuleDataDto) {
         boolean flag = false;
+        int integrationPointA = trapezodialRuleDataDto.getIntegrationPointA();
+        int integrationPointB = trapezodialRuleDataDto.getIntegrationPointB();
+        String problemString = trapezodialRuleDataDto.getProblemString();
+        int hParameter = trapezodialRuleDataDto.gethParameter();
+        int i = 0;
+
+        ArrayList<Double> listToCheck = new ArrayList<>();
+        for(i=integrationPointA;i<=integrationPointB;i++){
+            double x = i;
+            listToCheck.add(x);
+        }
+
 
         return false;
     }
