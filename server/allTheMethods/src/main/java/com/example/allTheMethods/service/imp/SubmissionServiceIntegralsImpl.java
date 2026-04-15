357a283ce103714d85c8891d758aee46a5719f2a
Sotos <tsompilis@gmail.com>
2026-04-15 15:13:07 +0300

    trapezodial rule finished needs testing

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImpl.java
@@ -3,11 +3,12 @@ package com.example.allTheMethods.service.imp;
 import com.example.allTheMethods.dto.TrapezodialRuleDataDto;
 import com.example.allTheMethods.service.SubmissionServiceIntegrals;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
+import static com.example.allTheMethods.service.imp.SubmissionServiceImpl.fx;
 
 @Service
 public class SubmissionServiceIntegralsImpl implements SubmissionServiceIntegrals {
+
     @Override
     public boolean checkTrapezodialRuleData(TrapezodialRuleDataDto trapezodialRuleDataDto) {
         boolean flag = false;
@@ -17,11 +18,21 @@ public class SubmissionServiceIntegralsImpl implements SubmissionServiceIntegral
         int hParameter = trapezodialRuleDataDto.gethParameter();
         int i = 0;
 
+        ArrayList<Double> generatedList = new ArrayList<>();
         ArrayList<Double> listToCheck = new ArrayList<>();
+        //Generate a list of the numbers used in the method
         for(i=integrationPointA;i<=integrationPointB;i++){
             double x = i;
-            listToCheck.add(x);
+            x = fx(x, problemString);
+            generatedList.add(x);
         }
+
+        //To avoid if, else if, else statement we implemented this version
+        for(i=0;i<generatedList.size();i++){
+            listToCheck.add(generatedList.get(i) * 2);
+        }
+        listToCheck.set(0, listToCheck.get(0)/2);
+        listToCheck.set(listToCheck.size()-1, listToCheck.get(listToCheck.size()-1)/2);
 
 
         return false;
