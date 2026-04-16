4be02bc5037b5e181c46c42914ac4b16a5f23e08
Sotos <tsompilis@gmail.com>
2026-04-16 14:53:42 +0300

    romberg data dto added

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/RombergDataDto.java
@@ -0,0 +1,12 @@
+package com.example.allTheMethods.dto;
+
+import java.util.List;
+
+public class RombergDataDto extends SubmissionDataDto{
+    public RombergDataDto(List<Double> inp, String problemString) {
+        super(inp, problemString);
+    }
+
+    public RombergDataDto() {
+    }
+}
