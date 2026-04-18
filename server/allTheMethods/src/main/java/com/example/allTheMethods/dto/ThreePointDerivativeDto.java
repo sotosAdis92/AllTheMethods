197e100c9e4121264f48fad98284efa58e3f5956
Sotos <tsompilis@gmail.com>
2026-04-18 19:19:59 +0300

    derivative three points implemented

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/ThreePointDerivativeDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/ThreePointDerivativeDto.java
@@ -4,20 +4,12 @@ import java.util.List;
 
 public class ThreePointDerivativeDto extends SubmissionDataDto{
     private List<Integer> coutingParameters;
-    private List<Double> xiParameters;
+    private List<Integer> xiParameters;
     private List<Integer> fiParameters;
     private double xoParameter;
     private String typeOfDerivative;
 
-    public ThreePointDerivativeDto(List<Double> inp, String problemString, List<Integer> coutingParameters, List<Double> xiParameters, List<Integer> fiParameters, double xoParameter) {
-        super(inp, problemString);
-        this.coutingParameters = coutingParameters;
-        this.xiParameters = xiParameters;
-        this.fiParameters = fiParameters;
-        this.xoParameter = xoParameter;
-    }
-
-    public ThreePointDerivativeDto(List<Double> inp, String problemString, List<Integer> coutingParameters, List<Double> xiParameters, List<Integer> fiParameters, double xoParameter, String typeOfDerivative) {
+    public ThreePointDerivativeDto(List<Double> inp, String problemString, List<Integer> coutingParameters, List<Integer> xiParameters, List<Integer> fiParameters, double xoParameter, String typeOfDerivative) {
         super(inp, problemString);
         this.coutingParameters = coutingParameters;
         this.xiParameters = xiParameters;
@@ -26,27 +18,11 @@ public class ThreePointDerivativeDto extends SubmissionDataDto{
         this.typeOfDerivative = typeOfDerivative;
     }
 
-    public void setCoutingParameters(List<Integer> coutingParameters) {
-        this.coutingParameters = coutingParameters;
-    }
-
-    public void setXiParameters(List<Double> xiParameters) {
-        this.xiParameters = xiParameters;
-    }
-
-    public void setFiParameters(List<Integer> fiParameters) {
-        this.fiParameters = fiParameters;
-    }
-
-    public void setXoParameter(double xoParameter) {
-        this.xoParameter = xoParameter;
-    }
-
     public List<Integer> getCoutingParameters() {
         return coutingParameters;
     }
 
-    public List<Double> getXiParameters() {
+    public List<Integer> getXiParameters() {
         return xiParameters;
     }
 
@@ -60,6 +36,22 @@ public class ThreePointDerivativeDto extends SubmissionDataDto{
 
     public String getTypeOfDerivative() {
         return typeOfDerivative;
+    }
+
+    public void setCoutingParameters(List<Integer> coutingParameters) {
+        this.coutingParameters = coutingParameters;
+    }
+
+    public void setXiParameters(List<Integer> xiParameters) {
+        this.xiParameters = xiParameters;
+    }
+
+    public void setFiParameters(List<Integer> fiParameters) {
+        this.fiParameters = fiParameters;
+    }
+
+    public void setXoParameter(double xoParameter) {
+        this.xoParameter = xoParameter;
     }
 
     public void setTypeOfDerivative(String typeOfDerivative) {
--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceDerivativesImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceDerivativesImpl.java
@@ -1,2 +1,61 @@
-package com.example.allTheMethods.service.imp;public class SubmissionServiceDerivativesImpl {
+package com.example.allTheMethods.service.imp;
+
+import com.example.allTheMethods.dto.FivePointDerivativeDto;
+import com.example.allTheMethods.dto.RichardsonDataDto;
+import com.example.allTheMethods.dto.ThreePointDerivativeDto;
+import com.example.allTheMethods.service.SubmissionServiceDerivatives;
+
+import java.util.List;
+
+public class SubmissionServiceDerivativesImpl implements SubmissionServiceDerivatives {
+    @Override
+    public boolean checkThreePointDerivativeData(ThreePointDerivativeDto threePointDerivativeDto) {
+        boolean flag = false;
+        String typeOfDerivative = threePointDerivativeDto.getTypeOfDerivative();
+        List<Integer> fiParameters = threePointDerivativeDto.getFiParameters();
+        List<Integer> xiParameters = threePointDerivativeDto.getXiParameters();
+        List<Double> userInputs = threePointDerivativeDto.getInp();
+        double xoParameter = threePointDerivativeDto.getXoParameter();
+        double hParameter = 0.0;
+        double fprime = 0.0;
+        double fsecondPrime = 0.0;
+        int indexOfXoParameter = 0;
+        int i = 0;
+        for(i = 0;i<xiParameters.size();i++){
+            hParameter = xiParameters.get(i) - xiParameters.get(i-1);
+            if(xiParameters.get(i) == xoParameter){
+                indexOfXoParameter = i;
+            }
+        }
+        System.out.println(hParameter);
+        System.out.println(indexOfXoParameter);
+        if(typeOfDerivative == "fprime"){
+            fprime = (-1 * f(indexOfXoParameter-1,fiParameters) + 0 * f(indexOfXoParameter,fiParameters) + 1 * f(indexOfXoParameter+1,fiParameters))/(2*hParameter);
+        }
+        else{
+            fsecondPrime = (1 * f(indexOfXoParameter-1,fiParameters) + (-2) * f(indexOfXoParameter,fiParameters) + 1 * f(indexOfXoParameter + 1,fiParameters))/(hParameter * hParameter);
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean checkFivePointDerivativeData(FivePointDerivativeDto fivePointDerivativeDto) {
+        return false;
+    }
+
+    @Override
+    public boolean checkRichardsonData(RichardsonDataDto richardsonDataDto) {
+        return false;
+    }
+
+    public int f(int indexOfXoParameter, List<Integer> fiParameters){
+        int x = 0;
+        int i = 0;
+        for(i = 0;i<fiParameters.size();i++){
+            if(i == indexOfXoParameter){
+                x = fiParameters.get(i);
+            }
+        }
+        return x;
+    }
 }
