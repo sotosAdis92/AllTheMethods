b6f046a630555d5d3002dc0939a132da10497886
Sotos <tsompilis@gmail.com>
2026-04-18 17:39:14 +0300

    5 point derivative dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/FivePointDerivativeDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/FivePointDerivativeDto.java
@@ -1,4 +1,50 @@
 package com.example.allTheMethods.dto;
 
+import java.util.List;
+
 public class FivePointDerivativeDto extends SubmissionDataDto{
+    private List<Integer> coutingParameters;
+    private List<Double> xiParameters;
+    private List<Integer> fiParameters;
+    private double xoParameter;
+
+    public FivePointDerivativeDto(List<Double> inp, String problemString, List<Integer> coutingParameters, List<Double> xiParameters, List<Integer> fiParameters, double xoParameter) {
+        super(inp, problemString);
+        this.coutingParameters = coutingParameters;
+        this.xiParameters = xiParameters;
+        this.fiParameters = fiParameters;
+        this.xoParameter = xoParameter;
+    }
+
+    public List<Integer> getCoutingParameters() {
+        return coutingParameters;
+    }
+
+    public void setCoutingParameters(List<Integer> coutingParameters) {
+        this.coutingParameters = coutingParameters;
+    }
+
+    public List<Double> getXiParameters() {
+        return xiParameters;
+    }
+
+    public void setXiParameters(List<Double> xiParameters) {
+        this.xiParameters = xiParameters;
+    }
+
+    public List<Integer> getFiParameters() {
+        return fiParameters;
+    }
+
+    public void setFiParameters(List<Integer> fiParameters) {
+        this.fiParameters = fiParameters;
+    }
+
+    public double getXoParameter() {
+        return xoParameter;
+    }
+
+    public void setXoParameter(double xoParameter) {
+        this.xoParameter = xoParameter;
+    }
 }
