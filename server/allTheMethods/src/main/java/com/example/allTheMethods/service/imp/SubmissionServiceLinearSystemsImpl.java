050882ed520998416a6e85be63cbaf54895025e5
Sotos <tsompilis@gmail.com>
2026-04-19 23:30:41 +0300

    preparing submission service for linear systems

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceLinearSystemsImpl.java
@@ -0,0 +1,8 @@
+package com.example.allTheMethods.service.imp;
+
+import com.example.allTheMethods.service.SubmissionServiceLinearSystems;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubmissionServiceLinearSystemsImpl implements SubmissionServiceLinearSystems {
+}
