14e302bff772021006395a994ffdeddaf99372fa
Sotos <tsompilis@gmail.com>
2026-04-19 17:49:29 +0300

    runge kutta four stage dto finished

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/RungeKuttaDataDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/RungeKuttaDataDto.java
@@ -1,7 +1,40 @@
 package com.example.allTheMethods.dto;
 
+import java.util.List;
+
 public class RungeKuttaDataDto extends SubmissionDataDto{
     private double hParameter;
     private double yZero;
     private double xZero;
+
+    public RungeKuttaDataDto(List<Double> inp, String problemString, double hParameter, double yZero, double xZero) {
+        super(inp, problemString);
+        this.hParameter = hParameter;
+        this.yZero = yZero;
+        this.xZero = xZero;
+    }
+
+    public double gethParameter() {
+        return hParameter;
+    }
+
+    public void sethParameter(double hParameter) {
+        this.hParameter = hParameter;
+    }
+
+    public double getyZero() {
+        return yZero;
+    }
+
+    public void setyZero(double yZero) {
+        this.yZero = yZero;
+    }
+
+    public double getxZero() {
+        return xZero;
+    }
+
+    public void setxZero(double xZero) {
+        this.xZero = xZero;
+    }
 }
