ea41ea7df8b2189f7b7cc157dd7473952e8875f2
Sotos <tsompilis@gmail.com>
2026-04-21 00:58:23 +0300

    starting fresh with new problem screen

--- clientside/src/Components/ProblemScreen/ProblemDescription.jsx
+++ clientside/src/Components/ProblemScreen/ProblemDescription.jsx
@@ -1,2 +1,20 @@
-const ProblemDescription = () => {};
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getProblem } from "../../services/ProblemService";
+const ProblemDescription = () => {
+  const { id } = useParams;
+  const [description, setDescription] = useState("");
+  const [problemString, setProblemString] = useState("");
+  const [category, setCategory] = useState("");
+  const [problemType, setProblemType] = useState("");
+  useEffect(() => {
+    getProblem(id)
+      .then((response) => {
+        setDescription(response.data.description);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+};
 export default ProblemDescription;
