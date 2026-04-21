856e51afd4a880af7f78b3b36217ae784cd5fa25
Sotos <tsompilis@gmail.com>
2026-04-21 22:40:08 +0300

    bisection method ui

--- clientside/src/Components/ProblemScreen/BisectionComponent.jsx
+++ clientside/src/Components/ProblemScreen/BisectionComponent.jsx
@@ -1,3 +1,16 @@
-const BisectionComponent = () => {};
+import { useEffect, useState } from "react";
+const BisectionComponent = () => {
+  const [description, setDescription] = useState("");
+  const [problemData, setProblemData] = useState("");
+  const [problemString, setProblemString] = useState("");
+  useEffect(() => {});
+  return (
+    <>
+      <div>{description}</div>
+      <div>{problemData}</div>
+      <div>{problemString}</div>
+    </>
+  );
+};
 
 export default BisectionComponent;
--- clientside/src/Components/ProblemScreen/ProblemDescription.jsx
+++ clientside/src/Components/ProblemScreen/ProblemDescription.jsx
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProblem } from "../../services/ProblemService";
+import BisectionComponent from "./BisectionComponent";
 const ProblemDescription = () => {
   const { id } = useParams();
   const [problemType, setProblemType] = useState("");
-  // const [problemData, setProblemData] = useState("");
   useEffect(() => {
     getProblem(id)
       .then((response) => {
         setProblemType(response.data.problemType);
-        // const parsedData = JSON.parse(response.data.problemData);
-        // setProblemData(parsedData);
-        // console.log(parsedData.iterations);
-        // console.log(parsedData.problemSpaceA);
-        // console.log(parsedData.problemSpaceB);
       })
       .catch((error) => {
         console.log(error);
       });
   }, [id]);
 
+  const renderProblem = (problemType) => {
+    if (problemType == "Bisection") {
+      return <BisectionComponent></BisectionComponent>;
+    }
+  };
+
   return (
     <>
-      <div>{problemType}</div>
+      <div>{renderProblem(problemType)}</div>
     </>
   );
 };
