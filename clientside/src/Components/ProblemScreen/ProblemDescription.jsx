c9daa3c9efec7cf14664b1934322ad7f962b1a4a
Sotos <tsompilis@gmail.com>
2026-04-21 20:00:53 +0300

    changing how we render problems

--- clientside/src/Components/ProblemScreen/ProblemDescription.jsx
+++ clientside/src/Components/ProblemScreen/ProblemDescription.jsx
@@ -3,20 +3,12 @@ import { useParams } from "react-router-dom";
 import { getProblem } from "../../services/ProblemService";
 const ProblemDescription = () => {
   const { id } = useParams();
-  const [description, setDescription] = useState("");
-  const [problemString, setProblemString] = useState("");
-  const [category, setCategory] = useState("");
   const [problemType, setProblemType] = useState("");
-  const [difficulty, setDifficulty] = useState("");
   // const [problemData, setProblemData] = useState("");
   useEffect(() => {
     getProblem(id)
       .then((response) => {
-        setDescription(response.data.description);
-        setProblemString(response.data.problemString);
         setProblemType(response.data.problemType);
-        setCategory(response.data.category);
-        setDifficulty(response.data.difficulty);
         // const parsedData = JSON.parse(response.data.problemData);
         // setProblemData(parsedData);
         // console.log(parsedData.iterations);
@@ -30,11 +22,7 @@ const ProblemDescription = () => {
 
   return (
     <>
-      <div>{description}</div>
-      <div>{problemString}</div>
-      <div>{category}</div>
       <div>{problemType}</div>
-      <div>{difficulty}</div>
     </>
   );
 };
