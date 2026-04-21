8db1f3cbed7ca6302fcf8046ee42d0bff996e145
Sotos <tsompilis@gmail.com>
2026-04-21 23:54:22 +0300

    adding the parameters into the bisection problems

--- clientside/src/Components/ProblemScreen/BisectionComponent.jsx
+++ clientside/src/Components/ProblemScreen/BisectionComponent.jsx
@@ -1,13 +1,30 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProblem } from "../../services/ProblemService";
 const BisectionComponent = () => {
+  const { id } = useParams();
   const [description, setDescription] = useState("");
   const [problemData, setProblemData] = useState("");
   const [problemString, setProblemString] = useState("");
-  useEffect(() => {});
+  useEffect(() => {
+    getProblem(id)
+      .then((response) => {
+        setDescription(response.data.description);
+        const problemDataConverted = JSON.parse(response.data.problemData);
+        setProblemData(problemDataConverted);
+        setProblemString(response.data.problemString);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
   return (
     <>
       <div>{description}</div>
-      <div>{problemData}</div>
+      <div>{problemData.iterations}</div>
+      <div>{problemData.problemSpaceA}</div>
+      <div>{problemData.problemSpaceB}</div>
       <div>{problemString}</div>
     </>
   );
