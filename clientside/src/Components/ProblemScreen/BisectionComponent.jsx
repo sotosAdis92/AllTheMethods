42a5beb59f27b9a79a69d4c5f1a57fc8116e17ad
Sotos <tsompilis@gmail.com>
2026-04-22 21:21:11 +0300

    finished input array

--- clientside/src/Components/ProblemScreen/BisectionComponent.jsx
+++ clientside/src/Components/ProblemScreen/BisectionComponent.jsx
@@ -12,7 +12,9 @@ const BisectionComponent = () => {
   const [problemSpaceA, setProblemSpaceA] = useState("");
   const [problemSpaceB, setProblemSpaceB] = useState("");
   const [usersId, setUsersId] = useState("");
-  var inputs = [];
+  const [input, setInput] = useState([]);
+  const [inputI, setInputI] = useState([]);
+  var inputs = []; //Create the array to store the input fields
   useEffect(() => {
     getProblem(id)
       .then((response) => {
@@ -38,7 +40,6 @@ const BisectionComponent = () => {
   });
   //Implement input generation based on how many iterations you have
   for (let i = 0; i < iterations; i++) {
-    console.log(i);
     inputs.push(
       <div key={i}>
         <input type="text" onChange={(e) => handleInput(i, e)}></input>
@@ -47,10 +48,23 @@ const BisectionComponent = () => {
   }
 
   //Function for putting the inputs in the correct place in the array
-  function handleInput(i, e) {}
+  function handleInput(i, e) {
+    const index = input.findIndex((item) => item[0] === i); //search the array to find if the index is in there
+    if (index !== -1) {
+      //if the index does already exist in the array
+      input[index] = [i, Number(e.target.value)]; //overright the previous value of it
+    } else {
+      //if it does not exist within the array
+      input.push([i, Number(e.target.value)]); //put the new value to the correct position
+    }
+    const inputI = input.map((index) => index[1]); //create a new array where only the values are, no indexes
+    setInput([...input]); //copy the indexes array
+    setInputI(inputI);
+    console.log(inputI);
+  }
   //Submitting data based on what method we have rendered to reduce if checks for both client and server
   const submissionData = {
-    inputs,
+    inputI,
     problemMethod,
     problemString,
     iterations,
@@ -79,7 +93,7 @@ const BisectionComponent = () => {
       <div>
         In the Space [{problemData.problemSpaceA},{problemData.problemSpaceB}]
       </div>
-      {handleInput()}
+      {inputs}
     </>
   );
 };
