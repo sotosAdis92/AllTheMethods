8b40b2845ec17a4915a8bcb5aa67550457992263
Sotos <tsompilis@gmail.com>
2026-04-23 20:26:23 +0300

    css for errors

--- clientside/src/Components/FormInput.jsx
+++ clientside/src/Components/FormInput.jsx
@@ -1,10 +1,11 @@
+import "../FormInputs.css";
 const FormInput = (props) => {
   const { label, onChange, errorMessage, id, i, ...inputProps } = props;
   return (
     <div className="formInput">
       <label>{label}</label>
       <input {...inputProps} onChange={onChange}></input>
-      <span>{errorMessage}</span>
+      <span className="errorForInputs">{errorMessage}</span>
     </div>
   );
 };
