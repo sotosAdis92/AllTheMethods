0363cf5dee146e6986981d71f98e8377a2a0d2af
Sotos <tsompilis@gmail.com>
2026-04-24 04:27:19 +0300

    achievement mapper implemented needs testing

--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserAchievementsController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserAchievementsController.java
@@ -1,11 +1,10 @@
 package com.example.allTheMethods.controllers;
 
+import com.example.allTheMethods.dto.UserAchievementDto;
 import com.example.allTheMethods.service.UserAchievementService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/userAchievements")
@@ -15,6 +14,12 @@ public class UserAchievementsController {
 
     public UserAchievementsController(UserAchievementService userAchievementService) {
         this.userAchievementService = userAchievementService;
+    }
+
+    @PostMapping("/saveAchievements")
+    public ResponseEntity<UserAchievementDto> saveUserAchievement(UserAchievementDto userAchievementDto){
+        UserAchievementDto userAchievementDto1 = userAchievementService.saveUserAchievements(userAchievementDto);
+        return new ResponseEntity<>(userAchievementDto1, HttpStatus.CREATED);
     }
 
     @GetMapping("/myAchievements")
--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserProblemController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserProblemController.java
@@ -1,12 +1,11 @@
 package com.example.allTheMethods.controllers;
 
+import com.example.allTheMethods.dto.UserProblemDto;
 import com.example.allTheMethods.service.UserProblemService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -16,6 +15,12 @@ public class UserProblemController {
 
     public UserProblemController(UserProblemService userProblemService) {
         this.userProblemService = userProblemService;
+    }
+
+    @PostMapping("/saveProblem")
+    public ResponseEntity<UserProblemDto> saveUserProblem(@RequestBody UserProblemDto userProblemDto){
+        UserProblemDto userProblemDto1 = userProblemService.saveUserProblem(userProblemDto);
+        return new ResponseEntity<>(userProblemDto1, HttpStatus.CREATED);
     }
 
     @GetMapping("/myProblems")
