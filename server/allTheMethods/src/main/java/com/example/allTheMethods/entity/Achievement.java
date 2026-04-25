2b6c806dfe4c0a2d4058e0188865cf988484e679
Sotos <tsompilis@gmail.com>
2026-04-25 03:02:09 +0300

    corrected mistake in achievement entity

--- server/allTheMethods/src/main/java/com/example/allTheMethods/entity/Achievement.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/entity/Achievement.java
@@ -26,9 +26,6 @@ public class Achievement {
     @Column(nullable = false)
     private String visibility;
 
-    @Column
-    private int counter;
-
     @OneToMany(mappedBy = "achievement")
     Set<UserAchievements> achievements;
 
@@ -45,15 +42,6 @@ public class Achievement {
         this.visibility = visibility;
     }
 
-    public Achievement(Long achievementId, String name, String description, String category, String rank, String visibility, int counter) {
-        this.achievementId = achievementId;
-        this.name = name;
-        this.description = description;
-        this.category = category;
-        this.rank = rank;
-        this.visibility = visibility;
-        this.counter = counter;
-    }
 
     public Achievement(Long achievementId, String name, String description, String category, String rank, String visibility, Set<UserAchievements> achievements) {
         this.achievementId = achievementId;
@@ -119,13 +107,5 @@ public class Achievement {
 
     public Set<UserAchievements> getAchievements() {
         return achievements;
-    }
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public void setCounter(int counter) {
-        this.counter = counter;
     }
 }
