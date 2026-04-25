914d30bcdd05c463aa133ae58004b593100ad5fd
Sotos <tsompilis@gmail.com>
2026-04-26 00:23:43 +0300

    removed unused inmports

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/UserAchievementServiceImp.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/UserAchievementServiceImp.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static java.lang.Integer.parseInt;
 
 @Service
 public class UserAchievementServiceImp implements UserAchievementService {
