11487bcb15f7771b427305337afb914125ffea4f
Sotos <tsompilis@gmail.com>
2026-06-22 21:06:03 +0300

    v0.1.6 general use objects for methods to reduce code repetition

---
+++ clientside/src/services/GeneralObjects.js
@@ -0,0 +1,129 @@
+import {
+  sendDiakritiNewtonRaphsonData,
+  sendNewtonRaphsonData,
+  sendRegulaFalsiData,
+  sendSimposonData,
+  sendSubmissionData,
+  sendTrapezodialData,
+} from "./SubmitService";
+
+export const problemObjectsConfig = {
+  Bisection: {
+    type: "Polynomial-Roots",
+    submitFunction: (data) => sendSubmissionData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      problemSpaceA: state.problemSpaceA,
+      problemSpaceB: state.problemSpaceB,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelSpace: `In the space [${state.problemSpaceA},${state.problemSpaceB}]`,
+    }),
+  },
+
+  "Regula-Falsi": {
+    type: "Polynomial-Roots",
+    submitFunction: (data) => sendRegulaFalsiData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      problemSpaceA: state.problemSpaceA,
+      problemSpaceB: state.problemSpaceB,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelSpace: `In the space [${state.problemSpaceA},${state.problemSpaceB}]`,
+    }),
+  },
+
+  "Newton-Raphson": {
+    type: "Polynomial-Roots",
+    submitFunction: (data) => sendNewtonRaphsonData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      xZero: state.xo,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelXo: `With an Xo=${state.xZero}`,
+    }),
+  },
+
+  "Diakriti-Newton-Raphson": {
+    type: "Polynomial-Roots",
+    submitFunction: (data) => sendDiakritiNewtonRaphsonData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      xZero: state.xo,
+      hparameter: state.hparameter,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelXo: `With an Xo=${state.xZero}`,
+      labelHparameter: `With an H=${state.hparameter}`,
+    }),
+  },
+
+  "Trapezodial-Rule": {
+    type: "Integrals",
+    submitFunction: (data) => sendTrapezodialData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      problemSpaceA: state.problemSpaceA,
+      problemSpaceB: state.problemSpaceB,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelSpace: `In the space [${state.problemSpaceA},${state.problemSpaceB}]`,
+    }),
+  },
+
+  "Simpson-Rule": {
+    type: "Integrals",
+    submitFunction: (data) => sendSimposonData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      problemSpaceA: state.problemSpaceA,
+      problemSpaceB: state.problemSpaceB,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelSpace: `In the space [${state.problemSpaceA},${state.problemSpaceB}]`,
+    }),
+  },
+
+  "Three-Point-Derivative": {
+    type: "Polynomial-Roots",
+    submitFunction: (data) => sendSubmissionData(data),
+    getSubmissionData: (state, props) => ({
+      inp: state.inp,
+      problemMethod: props.problemMethod,
+      problemString: props.problemString,
+      iterations: state.iterations,
+      problemSpaceA: state.problemSpaceA,
+      problemSpaceB: state.problemSpaceB,
+    }),
+    displayStats: (state) => ({
+      labelIterations: `For: $(state.iterations) iterations`,
+      labelSpace: `In the space [${state.problemSpaceA},${state.problemSpaceB}]`,
+    }),
+  },
+};
