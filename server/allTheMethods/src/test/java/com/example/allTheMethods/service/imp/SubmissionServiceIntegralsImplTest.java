bb058072ed61510e6f5921e35342acf1219ab3c3
Sotos <tsompilis@gmail.com>
2026-06-24 01:28:32 +0300

    v0.2.5 test written for the trapezodial rule

--- server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImplTest.java
+++ server/allTheMethods/src/test/java/com/example/allTheMethods/service/imp/SubmissionServiceIntegralsImplTest.java
@@ -1,18 +1,31 @@
 package com.example.allTheMethods.service.imp;
 
+import com.example.allTheMethods.ast.TokenizerException;
+import com.example.allTheMethods.dto.NewtonRaphsonDataDto;
 import com.example.allTheMethods.dto.TrapezodialRuleDataDto;
+import com.example.allTheMethods.service.SubmissionServiceIntegrals;
+import com.example.allTheMethods.service.SubmissionServicePolynomials;
 import com.example.allTheMethods.utils.MethodUtils;
 import org.junit.jupiter.api.Test;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubmissionServiceIntegralsImplTest {
     @Test
-    public void testingTrapezodalRuleTestCase(){
+    public void testingTrapezodalRuleTestCase() throws TokenizerException{
+        SubmissionServiceIntegralsImpl submissionServiceIntegrals = new SubmissionServiceIntegralsImpl();
         TrapezodialRuleDataDto trpdto = new TrapezodialRuleDataDto();
         List<Double> testingInputs = new ArrayList<>();
+        testingInputs.add(1.0);
+        try{
+        assertEquals(true, submissionServiceIntegrals.checkTrapezodialRuleData(trpdto));
+        } catch (IllegalArgumentException illegalArgumentException){
+          System.out.println("Token exception");
+        }
     }
 }
