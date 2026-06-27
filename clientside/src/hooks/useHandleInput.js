794f598d5fe8af0132390dd356d87e77f777c239
Sotos <tsompilis@gmail.com>
2026-06-28 00:30:26 +0300

    v0.3.0 finished the use handle input javascript function

--- clientside/src/hooks/useHandleInput.js
+++ clientside/src/hooks/useHandleInput.js
@@ -5,5 +5,39 @@ export default function useHandleInput() {
   const [inp, setInputI] = useState([]);
   const [generalError, setGeneralError] = useState("");
 
-  return { input, inp, generalError, setGeneralError, updateInput };
+  const handleInput = (i, e) => {
+    const value = Number(e.target.value);
+    const indexOfNumber = input.findIndex(
+      (inputtedNumber) => inputtedNumber[0] === i,
+    );
+
+    let updatedInput;
+    if (indexOfNumber !== -1) {
+      updatedInput = [...input];
+      updatedInput[indexOfNumber] = [i, value];
+    } else {
+      updatedInput = [...input, [i, value]];
+    }
+
+    updatedInput.sort();
+    const extractedValues = updatedInput.map((num) => num[1]);
+
+    setInput(updatedInput);
+    setInputI(extractedValues);
+
+    console.log(updatedInput);
+    console.log(extractedValues);
+
+    if (extractedValues.length >= 1) {
+      setGeneralError("");
+    }
+  };
+
+  return {
+    input,
+    inp,
+    generalError,
+    setGeneralError,
+    handleInput,
+  };
 }
