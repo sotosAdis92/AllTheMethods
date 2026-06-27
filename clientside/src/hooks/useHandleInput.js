7b4b29bcb628bd053c7d274595731b96907528d1
Sotos <tsompilis@gmail.com>
2026-06-28 00:25:36 +0300

    v0.3.0 added is button disabled hook

--- clientside/src/hooks/useHandleInput.js
+++ clientside/src/hooks/useHandleInput.js
@@ -4,4 +4,6 @@ export default function useHandleInput() {
   const [input, setInput] = useState([]);
   const [inp, setInputI] = useState([]);
   const [generalError, setGeneralError] = useState("");
+
+  return { input, inp, generalError, setGeneralError, updateInput };
 }
