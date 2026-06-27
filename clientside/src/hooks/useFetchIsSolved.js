5025332ea1bebc53a42a2e918c719e4dc51d8d9f
Sotos <tsompilis@gmail.com>
2026-06-27 23:45:18 +0300

    v0.3.0 added a disable button when solved hook

--- clientside/src/hooks/useFetchIsSolved.js
+++ clientside/src/hooks/useFetchIsSolved.js
@@ -1,1 +1,9 @@
-export default function useFetchIsSolved(props) {}
+import { useEffect } from "react";
+export default function useFetchIsSolved(props, setButtonDisabled) {
+  useEffect(() => {
+    if (props.isSolved) {
+      setButtonDisabled(true);
+    }
+  }, [props.isSolved, setButtonDisabled]);
+  return {};
+}
