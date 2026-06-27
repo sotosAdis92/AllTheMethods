ae5efcdcf0212dc95ea9286211b7d4d4e2d558a2
Sotos <tsompilis@gmail.com>
2026-06-27 22:37:53 +0300

    v0.3.0 added new custom react hooks to get the achievements

--- clientside/src/Components/ProblemScreen/BisectionComponent.jsx
+++ clientside/src/Components/ProblemScreen/BisectionComponent.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { getAchievementsByCategory } from "../../services/AchievementService";
+import useFetchSpaceProblems from "../../hooks/useFetchSpaceProblems";
 import { decideResultText } from "../../services/GeneralFunctions";
 import { getProblem } from "../../services/ProblemService";
 import {
@@ -28,6 +28,9 @@ const BisectionComponent = (props) => {
     entry: "",
   });
   var entries = [];
+
+  const { problemId, problemData, iterations, problemSpaceA, problemSpaceB } =
+    useFetchSpaceProblems();
 
   //Fetching the users id
   useEffect(() => {
