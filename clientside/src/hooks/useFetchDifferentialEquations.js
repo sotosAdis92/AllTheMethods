216070496084d46f607ba729ee97d58ce9fee40a
Sotos <tsompilis@gmail.com>
2026-06-29 00:48:01 +0300

    v0.3.3 fixing the ui of direct euler, refactoring the component to have less code

---
+++ clientside/src/hooks/useFetchDifferentialEquations.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProblem } from "../services/ProblemService";
+export default function useFetchDifferentialEquations() {
+  const { id } = useParams();
+  const [problemData, setProblemData] = useState("");
+  const [xZero, setProblemXoParameter] = useState(0);
+  const [yZero, setProblemYoParameter] = useState(0);
+  const [iterations, setIterations] = useState(0);
+  const [hParameter, setProblemHparameter] = useState(0);
+  const [problemId, setProblemId] = useState(0);
+  console.log(problemData);
+  //Fetching the problem
+  useEffect(() => {
+    getProblem(id)
+      .then((response) => {
+        setProblemId(response.data.problemId);
+        const problemDataConverted = JSON.parse(response.data.problemData);
+        setProblemData(problemDataConverted);
+        setIterations(problemDataConverted.iterations);
+        setProblemXoParameter(problemDataConverted.xZero);
+        setProblemYoParameter(problemDataConverted.yZero);
+        setProblemHparameter(problemDataConverted.hParameter);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
+
+  return { problemId, iterations, xZero, yZero, hParameter };
+}
