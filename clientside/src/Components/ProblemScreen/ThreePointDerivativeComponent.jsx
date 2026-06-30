1679c0ac4e7e0abc515729cfe7a863a0336dbe90
Sotos <tsompilis@gmail.com>
2026-06-30 17:48:38 +0300

    v0.3.8 refactored the entire three point derivative ui component so that it has less code to be more managable

--- clientside/src/Components/ProblemScreen/ThreePointDerivativeComponent.jsx
+++ clientside/src/Components/ProblemScreen/ThreePointDerivativeComponent.jsx
@@ -1,91 +1,64 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { getAchievementsByCategory } from "../../services/AchievementService";
+import useGenerateInputsDerivatives, {
+  default as useCreateListsForDerivatives,
+  default as useFetchDerivatives,
+} from "../../hooks/useFetchDerivatives";
+import useFetchIsSolved from "../../hooks/useFetchIsSolved";
+import useFetchRelatedAchievements from "../../hooks/useFetchRelatedAchivements";
+import useFetchUserId from "../../hooks/useFetchUserId";
+import useGetTimeAndDate from "../../hooks/useGetTimeAndDate";
+import useHandleInput from "../../hooks/useHandleInput";
+import useResultTextHook from "../../hooks/useResultTextHook";
+import useSaveAchievementOfUser from "../../hooks/useSaveAchievementOfUser";
+import useSaveSolvedProblem from "../../hooks/useSaveSolvedProblem";
+import useSetCallback from "../../hooks/useSetCallback";
 import { decideResultText } from "../../services/GeneralFunctions";
-import { getProblem } from "../../services/ProblemService";
 import {
   saveSubmission,
   sendThreePointDerivativeData,
 } from "../../services/SubmitService";
-import { saveUserAchievement } from "../../services/UserAchievementService";
-import { saveSolvedProblem } from "../../services/UserProblemService";
-import { getUser } from "../../services/UsersService";
 import FormInput from "../FormInput";
 
 const ThreePointDerivativeComponent = (props) => {
   const { id } = useParams();
-  const [problemData, setProblemData] = useState("");
-  const [xZero, setProblemXZeroParameter] = useState(0);
-  const [iterations, setIterations] = useState("");
-  const [userId, setUsersId] = useState("");
-  const [input, setInput] = useState([]);
-  const [inp, setInputI] = useState([]);
-  const [problemId, setProblemId] = useState(0);
-  const [generalError, setGeneralError] = useState("");
   const [result, setResult] = useState(false);
   const [resultText, setResultText] = useState("");
-  const [achievements, setAchievements] = useState([]);
   const [isButtonDisabled, setButtonDisabled] = useState(false);
-  const [countingParameters, setCoutingParameters] = useState([]);
-  const [xiParameters, setXiParameters] = useState([]);
-  const [fiParameters, setFiParameters] = useState([]);
-  const [typeOfDerivative, setTypeOfDerivative] = useState("");
   let text;
   const [values, setValues] = useState({
     entry: "",
   });
   var entries = [];
 
-  useEffect(() => {
-    getProblem(id).then((response) => {
-      const parsedData = JSON.parse(response.data.problemData);
-      setProblemData(parsedData);
-      setIterations(parsedData.iterations);
-      setProblemXZeroParameter(parsedData.problemXoParameter);
-      setCoutingParameters(parsedData.countingParameters);
-      setXiParameters(parsedData.xiParameters);
-      setFiParameters(parsedData.fiParameters);
-      setTypeOfDerivative(parsedData.typeOfDerivative);
-    });
-  });
+  const {
+    problemId,
+    problemData,
+    iterations,
+    xZero,
+    countingParameters,
+    xiParameters,
+    fiParameters,
+    typeOfDerivative,
+  } = useFetchDerivatives();
 
-  useEffect(() => {
-    getAchievementsByCategory(props.problemCategory)
-      .then((response) => {
-        console.log(response.data);
-        const fetchedData = [];
-        for (let i = 0; i < response.data.length; i++) {
-          const achievement = response.data[i];
-          fetchedData.push(achievement);
-        }
-        setAchievements(fetchedData);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [props.problemCategory]);
-
-  useEffect(() => {
-    if (props.isSolved) {
-      disableButton();
-    }
-  });
-  //Implement input generation based on how many iterations you have
-  for (let i = 0; i < 1; i++) {
-    entries.push({
-      id: i,
-      placeholder: `f`,
-      type: "number",
-      label: `f = `,
-      name: "",
-      i: { i },
-      required: true,
-    });
-  }
-
-  const disableButton = () => {
-    setButtonDisabled(true);
-  };
+  const { listOfCountingParameters, listOfFiParameters, listOfXiParameters } =
+    useCreateListsForDerivatives(
+      xiParameters,
+      fiParameters,
+      countingParameters,
+    );
+  const { input, inp, generalError, setGeneralError, handleInput } =
+    useHandleInput();
+  const { saveAchievementOfUser } = useSaveAchievementOfUser();
+  const { achievements } = useFetchRelatedAchievements(props);
+  const { userId } = useFetchUserId();
+  useFetchIsSolved(props.isSolved, setButtonDisabled);
+  const { decideToSaveSolvedProblem } = useSaveSolvedProblem();
+  const { submittedAt } = useGetTimeAndDate();
+  const { setCallback } = useSetCallback(props);
+  useResultTextHook(result);
+  useGenerateInputsDerivatives(entries);
 
   function validateForm() {
     let valid = true;
@@ -97,38 +70,6 @@ const ThreePointDerivativeComponent = (props) => {
     }
     return valid;
   }
-  //function that takes in an index and the inputted value and either when the user enters a new value puts it into the array or replaces it
-  //later it sorts it for the index value so that x0 = index 0 ... x1 = index 1 ... xn = index n
-  //creates a copy and saves it
-  function handleInput(i, e) {
-    const value = Number(e.target.value);
-    const indexOfNumber = input.findIndex(
-      (inputtedNumber) => inputtedNumber[0] === i,
-    );
-    if (indexOfNumber !== -1) {
-      input[indexOfNumber] = [i, value];
-    } else {
-      input.push([i, Number(e.target.value)]);
-    }
-    input.sort();
-    const inp = input.map((num) => num[1]);
-    setInput([...input]);
-    setInputI(inp);
-
-    console.log(input);
-    console.log(inp);
-
-    if (inp.length >= 1) {
-      setGeneralError("");
-    }
-  }
-
-  const d = new Date();
-  let date = d.toLocaleDateString();
-  let time = d.toLocaleTimeString();
-  let submittedAt = date + " " + time;
-
-  //Props passed in from parrent element
   let problemMethod = props.problemMethod;
   let problemString = props.problemString;
   let problemCategory = props.problemCategory;
@@ -164,108 +105,14 @@ const ThreePointDerivativeComponent = (props) => {
         console.log(response.data);
       });
       const response = sendThreePointDerivativeData(submissionData);
-      const resultOfFetch = response.data;
-      setResult(resultOfFetch);
-      await decideResultText(result);
-      await decideToSaveSolvedProblem(result);
-      await saveAchievementOfUser(result);
+      const result = response.data;
+      setResult(result);
+      decideResultText(result);
+      setCallback(result);
+      await decideToSaveSolvedProblem(result, savedProblem, setButtonDisabled);
+      await saveAchievementOfUser(result, achievements);
     }
   };
-
-  //Function to save or not to save the problem based on the result that is returned by the server
-  const decideToSaveSolvedProblem = (result) => {
-    if (result === true) {
-      saveSolvedProblem(savedProblem).then((response) => {
-        console.log(response.data);
-      });
-      disableButton();
-    } else {
-      return;
-    }
-  };
-
-  const saveAchievementOfUser = (result) => {
-    if (result === true) {
-      for (let i = 0; i < achievements.length; i++) {
-        setUsersId(
-          getUser()
-            .then((response) => {
-              setUsersId(response.data.id);
-            })
-            .catch((error) => {
-              console.log(error);
-            }),
-        );
-        setProblemId(
-          getProblem(id)
-            .then((response) => {
-              setProblemId(response.data.problemId);
-            })
-            .catch((error) => {
-              console.log(error);
-            }),
-        );
-        const problemInfo = {
-          userAchievementDto: {
-            achievementId: achievements[i].achievementId,
-            userId: userId,
-            category: achievements[i].category,
-          },
-          userProblemDto: {
-            userId: userId,
-            problemId: problemId,
-            category: achievements[i].category,
-          },
-          achievementDto: {
-            achievementId: achievements[i].achievementId,
-            name: achievements[i].name,
-            description: achievements[i].description,
-            category: achievements[i].category,
-            rank: achievements[i].rank,
-            visibility: achievements[i].visibility,
-            counter: achievements[i].counter,
-          },
-        };
-
-        console.log(achievements[i].achievementId + ": Achievement Id");
-        console.log(achievements[i].description);
-        console.log(achievements[i].rank);
-        console.log(achievements[i].visibility);
-        console.log(achievements[i].category);
-        console.log(achievements[i].counter);
-        console.log(achievements[i].name);
-        console.log(userId);
-        console.log(problemId);
-        saveUserAchievement(problemInfo)
-          .then((response) => {
-            console.log(response.data.counter);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    } else {
-      return;
-    }
-  };
-
-  const listOfXiParameters = xiParameters.map((parameter, i) => (
-    <div key={i} className="xiParameters">
-      {parameter}
-    </div>
-  ));
-
-  const listOfFiParameters = fiParameters.map((parameter, i) => (
-    <div key={i} className="fiParameters">
-      {parameter}
-    </div>
-  ));
-
-  const listOfCountingParameters = countingParameters.map((parameter, i) => (
-    <div key={i} className="countingParameters">
-      {parameter}
-    </div>
-  ));
 
   return (
     <>
