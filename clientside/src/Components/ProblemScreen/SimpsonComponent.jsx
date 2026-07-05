caa4cb37d5b37f3b17d620cff61401dab50aa05c
sotosAdis92 <tsompilis@gmail.com>
2026-07-05 17:11:02 +0300

    v0.9.4 refactored the simpson ui component

--- clientside/src/Components/ProblemScreen/SimpsonComponent.jsx
+++ clientside/src/Components/ProblemScreen/SimpsonComponent.jsx
@@ -1,28 +1,20 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getAchievementsByCategory } from "../../services/AchievementService";
-import { getProblem } from "../../services/ProblemService";
+import { useState } from "react";
+import useFetchIntegrals from "../../hooks/useFetchIntergrals";
+import useFetchIsSolved from "../../hooks/useFetchIsSolved";
+import useFetchRelatedAchievements from "../../hooks/useFetchRelatedAchivements";
+import useFetchUserId from "../../hooks/useFetchUserId";
+import useGenerateSimpsonInputs from "../../hooks/useGenerateSimpsonInputs";
+import useGetTimeAndDate from "../../hooks/useGetTimeAndDate";
+import useHandleInput from "../../hooks/useHandleInput";
+import useResultTextHook from "../../hooks/useResultTextHook";
+import useSaveAchievementOfUser from "../../hooks/useSaveAchievementOfUser";
+import useSaveSolvedProblem from "../../hooks/useSaveSolvedProblem";
+import useSetCallback from "../../hooks/useSetCallback";
 import { saveSubmission, sendSimposonData } from "../../services/SubmitService";
-import { saveUserAchievement } from "../../services/UserAchievementService";
-import { saveSolvedProblem } from "../../services/UserProblemService";
-import { getUser } from "../../services/UsersService";
 import FormInput from "../FormInput";
-
 const SimpsonComponent = (props) => {
-  const { id } = useParams();
-  const [problemData, setProblemData] = useState("");
-  const [iterations, setIterations] = useState("");
-  const [hParameter, setHparameter] = useState("");
-  const [integrationPointA, setIntegrationPointA] = useState("");
-  const [integrationPointB, setIntegrationPointB] = useState("");
-  const [userId, setUsersId] = useState("");
-  const [input, setInput] = useState([]);
-  const [inp, setInputI] = useState([]);
-  const [problemId, setProblemId] = useState(0);
-  const [generalError, setGeneralError] = useState("");
   const [result, setResult] = useState(false);
   const [resultText, setResultText] = useState("");
-  const [achievements, setAchievements] = useState([]);
   const [isButtonDisabled, setButtonDisabled] = useState(false);
   const [functionString, setFunctionString] = useState("");
   let text;
@@ -31,145 +23,34 @@ const SimpsonComponent = (props) => {
   });
   var entries = [];
 
-  console.log(id);
-  useEffect(() => {
-    getProblem(id).then((response) => {
-      console.log(response.data.problemId);
-      setProblemId(response.data.problemId);
-      setFunctionString(response.data.functionString);
-      const problemDataParsed = JSON.parse(response.data.problemData);
-      setProblemData(problemDataParsed);
-      setHparameter(problemDataParsed.hParameter);
-      setIntegrationPointA(problemDataParsed.integrationSpaceA);
-      setIntegrationPointB(problemDataParsed.integrationSpaceB);
-      console.log(problemDataParsed.hParameter);
-    });
-  }, [id]);
-
-  useEffect(() => {
-    getAchievementsByCategory(props.problemCategory)
-      .then((response) => {
-        console.log(response.data);
-        const fetchedData = [];
-        for (let i = 0; i < response.data.length; i++) {
-          const achievement = response.data[i];
-          fetchedData.push(achievement);
-        }
-        setAchievements(fetchedData);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [props.problemCategory]);
-  console.log(achievements);
-
-  useEffect(() => {
-    getUser().then((response) => {
-      setUsersId(response.data.id);
-    });
-  });
-
-  useEffect(() => {
-    if (props.isSolved) {
-      disableButton();
-    }
-  });
-
-  const disableButton = () => {
-    setButtonDisabled(true);
-  };
+  const {
+    problemId,
+    problemData,
+    hParameter,
+    integrationSpaceA,
+    integrationSpaceB,
+  } = useFetchIntegrals();
+  const { achievements } = useFetchRelatedAchievements(props);
+  const { userId } = useFetchUserId();
+  useFetchIsSolved(props.isSolved, setButtonDisabled);
+  const { input, inp, generalError, setGeneralError, handleInput } =
+    useHandleInput();
+  const { saveAchievementOfUser } = useSaveAchievementOfUser();
+  const { decideToSaveSolvedProblem } = useSaveSolvedProblem();
+  const { submittedAt } = useGetTimeAndDate();
+  const { setCallback } = useSetCallback(props);
+  useResultTextHook(result);
+  useGenerateSimpsonInputs(entries, integrationSpaceA, integrationSpaceB);
 
   function validateForm() {
     let valid = true;
-    if (inp.length != iterations || text === 0) {
+    if (inp.length != integrationSpaceB - integrationSpaceA || text === 0) {
       valid = false;
       setGeneralError("One or more inputs are empty");
     } else {
       setGeneralError("");
     }
     return valid;
-  }
-
-  //function that takes in an index and the inputted value and either when the user enters a new value puts it into the array or replaces it
-  //later it sorts it for the index value so that x0 = index 0 ... x1 = index 1 ... xn = index n
-  //creates a copy and saves it
-  function handleInput(i, e) {
-    const value = Number(e.target.value);
-    const indexOfNumber = input.findIndex(
-      (inputtedNumber) => inputtedNumber[0] === i,
-    );
-    if (indexOfNumber !== -1) {
-      input[indexOfNumber] = [i, value];
-    } else {
-      input.push([i, Number(e.target.value)]);
-    }
-    input.sort();
-    const inp = input.map((num) => num[1]);
-    setInput([...input]);
-    setInputI(inp);
-
-    console.log(input);
-    console.log(inp);
-
-    if (inp.length >= 1) {
-      setGeneralError("");
-    }
-  }
-
-  //Setting up the local time objects for the submission
-  const d = new Date();
-  let date = d.toLocaleDateString();
-  let time = d.toLocaleTimeString();
-  let submittedAt = date + " " + time;
-
-  //Implement input generation based on how many iterations you have
-  for (let i = integrationPointA; i <= integrationPointB + 1; i++) {
-    if (i === integrationPointB + 1) {
-      entries.push({
-        id: i,
-        placeholder: `Final`,
-        label: `S = `,
-        name: "",
-        i: { i },
-        required: true,
-      });
-    } else {
-      if (i % 2 != 0 && i != integrationPointA && i != integrationPointB) {
-        entries.push({
-          id: i,
-          placeholder: `2f(${i})`,
-          type: "number",
-          label: `2f(${i}) = `,
-          name: "",
-          i: { i },
-          required: true,
-        });
-      } else if (
-        i % 2 === 0 &&
-        i != integrationPointA &&
-        i != integrationPointB
-      ) {
-        entries.push({
-          id: i,
-          placeholder: `4f(${i})`,
-          type: "number",
-          label: `4f(${i}) = `,
-          name: "",
-          i: { i },
-          required: true,
-        });
-      } else {
-        entries.push({
-          id: i,
-          placeholder: `f(${i})`,
-          type: "number",
-          label: `f(${i}) = `,
-          name: "",
-          i: { i },
-          required: true,
-        });
-      }
-    }
   }
 
   //Props passed in from parrent element
@@ -182,8 +63,8 @@ const SimpsonComponent = (props) => {
     problemMethod,
     problemString,
     hParameter,
-    integrationPointA,
-    integrationPointB,
+    integrationSpaceA,
+    integrationSpaceB,
   };
 
   const submission = {
@@ -207,91 +88,8 @@ const SimpsonComponent = (props) => {
       const result = response.data;
       setResult(result);
       setCallback(result);
-      await decideToSaveSolvedProblem(result);
-      await saveAchievementOfUser(result);
-    }
-  };
-
-  const setCallback = (result) => {
-    if (props.onResultReceived) {
-      props.onResultReceived(result);
-    }
-  };
-
-  //Function to save or not to save the problem based on the result that is returned by the server
-  const decideToSaveSolvedProblem = (result) => {
-    if (result === true) {
-      saveSolvedProblem(savedProblem).then((response) => {
-        console.log(response.data);
-      });
-      disableButton();
-    } else {
-      return;
-    }
-  };
-
-  const saveAchievementOfUser = (result) => {
-    if (result === true) {
-      for (let i = 0; i < achievements.length; i++) {
-        setUsersId(
-          getUser()
-            .then((response) => {
-              setUsersId(response.data.id);
-            })
-            .catch((error) => {
-              console.log(error);
-            }),
-        );
-        setProblemId(
-          getProblem(id)
-            .then((response) => {
-              setProblemId(response.data.problemId);
-            })
-            .catch((error) => {
-              console.log(error);
-            }),
-        );
-        const problemInfo = {
-          userAchievementDto: {
-            achievementId: achievements[i].achievementId,
-            userId: userId,
-            category: achievements[i].category,
-          },
-          userProblemDto: {
-            userId: userId,
-            problemId: problemId,
-            category: achievements[i].category,
-          },
-          achievementDto: {
-            achievementId: achievements[i].achievementId,
-            name: achievements[i].name,
-            description: achievements[i].description,
-            category: achievements[i].category,
-            rank: achievements[i].rank,
-            visibility: achievements[i].visibility,
-            counter: achievements[i].counter,
-          },
-        };
-
-        console.log(achievements[i].achievementId + ": Achievement Id");
-        console.log(achievements[i].description);
-        console.log(achievements[i].rank);
-        console.log(achievements[i].visibility);
-        console.log(achievements[i].category);
-        console.log(achievements[i].counter);
-        console.log(achievements[i].name);
-        console.log(userId);
-        console.log(problemId);
-        saveUserAchievement(problemInfo)
-          .then((response) => {
-            console.log(response.data.counter);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    } else {
-      return;
+      await decideToSaveSolvedProblem(result, savedProblem, setButtonDisabled);
+      await saveAchievementOfUser(result, achievements);
     }
   };
 
