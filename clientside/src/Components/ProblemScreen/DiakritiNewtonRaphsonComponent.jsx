c4717dbd44c8f77b678b86368f425fd0b182203e
sotosAdis92 <tsompilis@gmail.com>
2026-07-05 19:50:12 +0300

    v0.9.4 fixing the inputs

--- clientside/src/Components/ProblemScreen/DiakritiNewtonRaphsonComponent.jsx
+++ clientside/src/Components/ProblemScreen/DiakritiNewtonRaphsonComponent.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useImperativeHandle, useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetchIsSolved from "../../hooks/useFetchIsSolved";
 import useFetchRelatedAchievements from "../../hooks/useFetchRelatedAchivements";
