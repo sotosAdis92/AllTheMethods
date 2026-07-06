5b6b4f7124037e5ea5a59f15877dfe309fbed3f4
sotosAdis92 <tsompilis@gmail.com>
2026-07-06 19:17:55 +0300

    v0.10.2 added error message to the inputs

--- clientside/src/hooks/useGenerateInputsBisection.js
+++ clientside/src/hooks/useGenerateInputsBisection.js
@@ -5,6 +5,8 @@ export default function useGenerateInputsBisection(iterations, entries) {
       placeholder: `x${i}`,
       type: "number",
       label: `x_{${i}} = `,
+      errorMessage:
+        "Input Should not be empty or other than a floating point/double number",
       name: "",
       i: { i },
       required: true,
