76bf83559e7d8b8a7291ed78f561f607411f83fd
sotosAdis92 <tsompilis@gmail.com>
2026-07-06 15:27:07 +0300

    v0.10.1 removed unused css file for form inputs, creating a new one

--- clientside/src/Components/FormInput.jsx
+++ clientside/src/Components/FormInput.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
-import "../FormInputs.css";
 import KatexLabel from "./ProblemScreen/KatexLabel";
+import "./formInput.css";
 const FormInput = (props) => {
   const { label, onChange, errorMessage, ...inputProps } = props;
   const [focused, setFocused] = useState(false);
+
   const handleFocus = (e) => {
     e.preventDefault;
     setFocused(true);
   };
+
   console.log(props.isSolved);
+
   return (
     <div className="formInput">
       <KatexLabel latex={label}></KatexLabel>
