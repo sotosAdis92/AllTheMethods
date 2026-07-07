589f003439c7708675daaeba7acb32f0d99bb22c
sotosAdis92 <tsompilis@gmail.com>
2026-07-07 17:39:28 +0300

    v0.10.3 removed unused routes

--- clientside/src/Components/ListProblems.jsx
+++ clientside/src/Components/ListProblems.jsx
@@ -139,7 +139,6 @@ const ListProblems = () => {
             onClick={() => setOpenFilterBool(!openFilterBool)}
           >
             <img src={img3}></img>
-            Filters
           </button>
           {!openFilterBool ? (
             <div></div>
