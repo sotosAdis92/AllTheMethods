95e922d2b111bd0c038178dcd7e04a5e5e3b6808
sotosAdis92 <tsompilis@gmail.com>
2026-07-08 23:55:41 +0300

    v0.10.6 adding a new user profile image next to the display name

---
+++ clientside/src/Components/UserComponents/UserIcon.jsx
@@ -0,0 +1,11 @@
+const UserIcon = (props) => {
+  return (
+    <>
+      <div className="userDetails">
+        <img src=""></img>
+        <div>{props.displayName}</div>
+      </div>
+    </>
+  );
+};
+export default UserIcon;
