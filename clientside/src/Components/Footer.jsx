23399280872e4424775b80810d340c43717482e3
sotosAdis92 <tsompilis@gmail.com>
2026-07-09 11:35:36 +0300

    v0.11.1 added the all rights reserved to the footer

--- clientside/src/Components/Footer.jsx
+++ clientside/src/Components/Footer.jsx
@@ -1,2 +1,11 @@
-const Footer = () => {};
+const Footer = () => {
+  return (
+    <>
+      <div className="containerFooter">
+        <div className="rights">All Rights Reserved</div>
+        <div className="developerName">@sotosAdis92</div>
+      </div>
+    </>
+  );
+};
 export default Footer;
