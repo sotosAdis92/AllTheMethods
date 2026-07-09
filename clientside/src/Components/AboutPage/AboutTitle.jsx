1e6ea1692b387f7dc9e4476f81296126c658edc7
sotosAdis92 <tsompilis@gmail.com>
2026-07-09 03:37:06 +0300

    v0.11.0 added css styling to the title and subtitle

--- clientside/src/Components/AboutPage/AboutTitle.jsx
+++ clientside/src/Components/AboutPage/AboutTitle.jsx
@@ -3,7 +3,7 @@ const AboutTitle = () => {
   return (
     <>
       <div className="aboutTitle">
-        <h2 className="title">All The Methods</h2>
+        <h2 className="MainTitle">All The Methods</h2>
         <h4 className="subtitle">Learning Mathematics Gamified</h4>
       </div>
     </>
