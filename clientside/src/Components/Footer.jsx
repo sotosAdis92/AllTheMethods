6e2d7cd2b497d8669b0307503e0b4a716b0bfbd3
sotosAdis92 <tsompilis@gmail.com>
2026-07-10 03:17:55 +0300

    v0.11.0 added new css to footer

--- clientside/src/Components/Footer.jsx
+++ clientside/src/Components/Footer.jsx
@@ -5,21 +5,26 @@ const Footer = () => {
   return (
     <>
       <div className="containerFooter">
-        <div className="nameOfApp">
-          <img></img>
-          <h3>All The Methods</h3>
-        </div>
-        <div className="footerDesc">
-          <p>
-            Learn numerical analysis algorithms and methods through solving
-            problems and earning medals in a gamified experience.
-          </p>
-        </div>
-        <div className="reserved">
-          <div className="rights">
-            ©{year} AllTheMethods. All Rights Reserved
+        <div className="footerContent">
+          <div className="footerTop">
+            <div className="nameOfApp">
+              <img></img>
+              <h3>All The Methods</h3>
+            </div>
+            <div className="footerDesc">
+              <p>
+                Learn numerical analysis algorithms and methods through solving
+                problems and earning medals in a gamified experience.
+              </p>
+            </div>
           </div>
-          <div className="developerName">Created by sotosAdis92</div>
+
+          <div className="reserved">
+            <div className="rights">
+              ©{year} AllTheMethods. All Rights Reserved
+            </div>
+            <div className="developerName">Created by sotosAdis92</div>
+          </div>
         </div>
       </div>
     </>
