86fc12f0bad85d80660c0c20b01b57f2945c441a
sotosAdis92 <tsompilis@gmail.com>
2026-07-10 14:07:21 +0300

    v0.11.1 added the logo to the main about page

--- clientside/src/Components/AboutPage/AboutTitle.jsx
+++ clientside/src/Components/AboutPage/AboutTitle.jsx
@@ -3,7 +3,10 @@ const AboutTitle = () => {
   return (
     <>
       <div className="aboutTitle">
-        <h2 className="MainTitle">All The Methods</h2>
+        <div className="aboutImageWithHeading">
+          <img></img>
+          <h2 className="MainTitle">All The Methods</h2>
+        </div>
         <h4 className="subtitle">Learning Mathematics Gamified</h4>
       </div>
     </>
