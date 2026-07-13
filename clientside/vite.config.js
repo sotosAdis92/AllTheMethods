aba9ee047bc3eaf8d59f3b78d40c9a1cc7587e17
sotosAdis92 <tsompilis@gmail.com>
2026-07-13 15:35:21 +0300

    v0.12.1 fixed bugs related to the creation of new docker image

--- clientside/vite.config.js
+++ clientside/vite.config.js
@@ -5,4 +5,11 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
