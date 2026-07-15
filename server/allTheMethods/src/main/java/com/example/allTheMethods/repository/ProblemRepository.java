40c9f18b137e812f25261306dd3de755b09c0d56
sotosAdis92 <tsompilis@gmail.com>
2026-07-16 00:50:58 +0300

    removed unused functions

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
@@ -3,6 +3,7 @@ package com.example.allTheMethods.repository;
 import com.example.allTheMethods.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.HashMap;
 import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
