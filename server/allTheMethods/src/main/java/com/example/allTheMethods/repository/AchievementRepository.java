61baaeda8dc21e1e58ecf5c748c6ff3df55745be
sotosAdis92 <tsompilis@gmail.com>
2026-07-16 04:25:30 +0300

    added filters for category and rank to achievements repository

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/AchievementRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/AchievementRepository.java
@@ -3,10 +3,18 @@ package com.example.allTheMethods.repository;
 import com.example.allTheMethods.dto.AchievementDto;
 import com.example.allTheMethods.entity.Achievement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface AchievementRepository extends JpaRepository<Achievement, Long> {
     List<Achievement> findAchievementByCategory(String category);
     List<Achievement> findAchievementByRank(String rank);
+
+    @Query("SELECT a from Achievement a WHERE a.category IN (:categories) AND a.rank IN(:ranks)")
+    List<Achievement> findAchievementsByCategoryAndRank(@Param("categories") List<String> categories, @Param("ranks") List<String> ranks);
+
+    List<Achievement> findAchievementsByCategoryIn(@Param("categories") List<String> categories);
+    List<Achievement> findAchievementsByRankIn(@Param("ranks") List<String> ranks);
 }
--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
@@ -11,8 +11,10 @@ import java.util.List;
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
     List<Problem> findProblemByCategory(String category);
     List<Problem> findProblemByDifficulty(String difficulty);
+
     @Query("SELECT p FROM Problem p WHERE p.category IN (:categories) AND p.difficulty IN (:difficulties)")
     List<Problem> findProblemsByCategoryOrDifficulty(@Param("categories") List<String> categories, @Param("difficulties") List<String> difficulties);
+
     List<Problem> findProblemsByCategoryIn(@Param("categories") List<String> categories);
     List<Problem> findProblemsByDifficultyIn(@Param("difficulties") List<String> difficulties);
 }
