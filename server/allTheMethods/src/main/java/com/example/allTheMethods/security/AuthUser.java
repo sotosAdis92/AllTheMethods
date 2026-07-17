22ad034cbae93ca0f6a8fd15814f76ed9e550e41
sotosAdis92 <tsompilis@gmail.com>
2026-07-17 17:16:45 +0300

    added auth user

--- server/allTheMethods/src/main/java/com/example/allTheMethods/security/AuthUser.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/security/AuthUser.java
@@ -1,2 +1,17 @@
-package com.example.allTheMethods.security;public class AuthUser {
+package com.example.allTheMethods.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class AuthUser extends User {
+    private final Long id;
+    public AuthUser(Long id, String username, String password, String displayName, Collection<? extends GrantedAuthority> authorities){
+        super(username, password,authorities);
+        this.id = id;
+    }
+    public Long getId(){
+        return id;
+    }
 }
