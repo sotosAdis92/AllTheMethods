2aafde42abed25c05cb5457561a159f291eda7c1
sotosAdis92 <tsompilis@gmail.com>
2026-07-19 01:36:45 +0300

    added lombok annotations to dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/RungeKuttaDataDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/RungeKuttaDataDto.java
@@ -1,7 +1,12 @@
 package com.example.allTheMethods.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class RungeKuttaDataDto extends SubmissionDataDto{
     private int iterations;
     private double hParameter;
@@ -13,38 +18,6 @@ public class RungeKuttaDataDto extends SubmissionDataDto{
         this.iterations = iterations;
         this.hParameter = hParameter;
         this.yZero = yZero;
-        this.xZero = xZero;
-    }
-
-    public int getIterations() {
-        return iterations;
-    }
-
-    public void setIterations(int iterations) {
-        this.iterations = iterations;
-    }
-
-    public double gethParameter() {
-        return hParameter;
-    }
-
-    public void sethParameter(double hParameter) {
-        this.hParameter = hParameter;
-    }
-
-    public double getyZero() {
-        return yZero;
-    }
-
-    public void setyZero(double yZero) {
-        this.yZero = yZero;
-    }
-
-    public double getxZero() {
-        return xZero;
-    }
-
-    public void setxZero(double xZero) {
         this.xZero = xZero;
     }
 }
