1ec996bfb468da5ce581d390daee07a7a55e3869
sotosAdis92 <tsompilis@gmail.com>
2026-07-19 01:50:21 +0300

    added lombok annotations to dto three point derivative

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/ThreePointDerivativeDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/ThreePointDerivativeDto.java
@@ -1,7 +1,12 @@
 package com.example.allTheMethods.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class ThreePointDerivativeDto extends SubmissionDataDto{
     private List<Integer> coutingParameters;
     private List<Integer> xiParameters;
@@ -15,46 +20,6 @@ public class ThreePointDerivativeDto extends SubmissionDataDto{
         this.xiParameters = xiParameters;
         this.fiParameters = fiParameters;
         this.xoParameter = xoParameter;
-        this.typeOfDerivative = typeOfDerivative;
-    }
-
-    public List<Integer> getCoutingParameters() {
-        return coutingParameters;
-    }
-
-    public List<Integer> getXiParameters() {
-        return xiParameters;
-    }
-
-    public List<Integer> getFiParameters() {
-        return fiParameters;
-    }
-
-    public double getXoParameter() {
-        return xoParameter;
-    }
-
-    public String getTypeOfDerivative() {
-        return typeOfDerivative;
-    }
-
-    public void setCoutingParameters(List<Integer> coutingParameters) {
-        this.coutingParameters = coutingParameters;
-    }
-
-    public void setXiParameters(List<Integer> xiParameters) {
-        this.xiParameters = xiParameters;
-    }
-
-    public void setFiParameters(List<Integer> fiParameters) {
-        this.fiParameters = fiParameters;
-    }
-
-    public void setXoParameter(double xoParameter) {
-        this.xoParameter = xoParameter;
-    }
-
-    public void setTypeOfDerivative(String typeOfDerivative) {
         this.typeOfDerivative = typeOfDerivative;
     }
 }
