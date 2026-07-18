4b2fb0b2417eb6c83d26a093a3f181f1ec85bfa9
sotosAdis92 <tsompilis@gmail.com>
2026-07-18 16:55:52 +0300

    added lombock annotations to the achievement entity class

--- server/allTheMethods/src/main/java/com/example/allTheMethods/entity/Achievement.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/entity/Achievement.java
@@ -1,10 +1,15 @@
 package com.example.allTheMethods.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "achievement")
 public class Achievement {
     @Id
@@ -32,30 +37,6 @@ public class Achievement {
     @OneToMany(mappedBy = "achievement")
     Set<UserAchievements> achievements;
 
-
-    public Achievement() {
-    }
-
-    public Achievement(Long achievementId, String name, String description, String category, String rank, String visibility) {
-        this.achievementId = achievementId;
-        this.name = name;
-        this.description = description;
-        this.category = category;
-        this.rank = rank;
-        this.visibility = visibility;
-    }
-
-
-    public Achievement(Long achievementId, String name, String description, String category, String rank, String visibility, Set<UserAchievements> achievements) {
-        this.achievementId = achievementId;
-        this.name = name;
-        this.description = description;
-        this.category = category;
-        this.rank = rank;
-        this.visibility = visibility;
-        this.achievements = achievements;
-    }
-
     public Achievement(Long achievementId, String name, String description, String category, String rank, String visibility, int counter) {
         this.achievementId = achievementId;
         this.name = name;
@@ -63,70 +44,6 @@ public class Achievement {
         this.category = category;
         this.rank = rank;
         this.visibility = visibility;
-        this.counter = counter;
-    }
-
-    public Long getAchievementId() {
-        return achievementId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public String getVisibility() {
-        return visibility;
-    }
-
-    public void setAchievementId(Long achievementId) {
-        this.achievementId = achievementId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-
-    public void setVisibility(String visibility) {
-        this.visibility = visibility;
-    }
-
-    public void setAchievements(Set<UserAchievements> achievements) {
-        this.achievements = achievements;
-    }
-
-    public Set<UserAchievements> getAchievements() {
-        return achievements;
-    }
-
-    public int getCounter() {
-        return counter;
-    }
-
-    public void setCounter(int counter) {
         this.counter = counter;
     }
 }
