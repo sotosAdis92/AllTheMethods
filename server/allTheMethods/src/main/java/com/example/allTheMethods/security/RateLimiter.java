b63f765ad4999837343291af0e5163ca0dcf44fa
sotosAdis92 <tsompilis@gmail.com>
2026-07-21 14:46:57 +0300

    added caffeine concurrent caching library in pom.xml

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/security/RateLimiter.java
@@ -0,0 +1,20 @@
+package com.example.allTheMethods.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.rmi.server.ServerCloneException;
+
+public class RateLimiter extends OncePerRequestFilter {
+    private static final String LOGIN_URI = "/api/auth/login";
+    private static final String SUBMIT_URI = "/api/submissions/**";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain webFilterChain) throws ServletException, IOException {
+
+    }
+}
