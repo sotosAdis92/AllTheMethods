8eeec87e2d33bc626f32aa801379a4129eb3a70e
sotosAdis92 <tsompilis@gmail.com>
2026-07-21 15:02:16 +0300

    added rate limiter, overrid do filter internal method, and added path uris to the rate limiter in security

--- server/allTheMethods/src/main/java/com/example/allTheMethods/security/RateLimiter.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/security/RateLimiter.java
@@ -1,5 +1,9 @@
 package com.example.allTheMethods.security;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,7 +11,10 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+
 import java.rmi.server.ServerCloneException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class RateLimiter extends OncePerRequestFilter {
     private static final String LOGIN_URI = "/api/auth/login";
@@ -17,4 +24,5 @@ public class RateLimiter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain webFilterChain) throws ServletException, IOException {
 
     }
+
 }
