5bea905409eb03de1b8728f67c7c0a53f26b7be8
sotosAdis92 <tsompilis@gmail.com>
2026-07-27 03:57:12 +0300

    added list of submissions of the user in the client

--- clientside/src/Components/UserComponents/ViewMySubmissionsHistory.jsx
+++ clientside/src/Components/UserComponents/ViewMySubmissionsHistory.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import "../../App.css";
 import { getAllUserSubmissions } from "../../services/SubmitService";
 import ProblemDifficulty from "../ProblemDifficulty";
-
 const ViewMySumbissionsHistory = (props) => {
   const [mySubmissions, setMySubmissions] = useState([]);
   const userId = props.userId;
+  const count = mySubmissions.filter((mySubmission) => mySubmission[0]).length;
   const getAllSubmissions = () => {
     getAllUserSubmissions(userId)
       .then((response) => {
@@ -33,7 +34,22 @@ const ViewMySumbissionsHistory = (props) => {
 
   return (
     <>
-      <div>{listOfMySubmissions}</div>
+      {count > 0 ? (
+        <>
+          <div className="submissionsContainer">
+            <h1 className="submissionsHeader">Submission History</h1>
+            <div className="submissionsCounter">Total Submissions: {count}</div>
+            <ol className="listOfUserSubmissions">{listOfMySubmissions}</ol>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1 className="userProblemsHeader">Submission History</h1>
+          <div className="userProblemsCounter">Total Submissions: {count}</div>
+          <img className="countZeroImage"></img>
+          <p className="noProblems">No Submissions Yet!!</p>
+        </>
+      )}
     </>
   );
 };
