375e246bab39c60b12b8ba41ab45bf2a3d02fc55
sotosAdis92 <tsompilis@gmail.com>
2026-07-29 04:05:08 +0300

    created the create submission request dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateSubmissionRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.allTheMethods.dto.request;
-
-public record CreateSubmissionRequestDto(
-        Long id,
-        Long userId,
-        Long problemId,
-        String submittedAt
-) {
-}
