3f1d6c11cf2ec3e1840dbe896ccf38ad5a27c390
sotosAdis92 <tsompilis@gmail.com>
2026-07-31 01:00:15 +0300

    added achievement response dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/AchievementResponseDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/AchievementResponseDto.java
@@ -1,6 +1,12 @@
 package com.example.allTheMethods.dto.response;
 
 public record AchievementResponseDto(
-
+    Long id,
+    String name,
+    String description,
+    String category,
+    String rank,
+    String visibility,
+    int counter
 ) {
 }
