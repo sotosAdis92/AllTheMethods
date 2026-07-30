20aea6e9d59816fa1d652d05ea3d7484b22ae1b9
sotosAdis92 <tsompilis@gmail.com>
2026-07-30 03:18:14 +0300

    removed unused user constructor

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/UsersDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/UsersDto.java
@@ -22,12 +22,6 @@ public class UsersDto {
         this.userRole = userRole;
     }
 
-    public UsersDto(Long id, String username, String displayName) {
-        this.id = id;
-        this.username = username;
-        this.displayName = displayName;
-    }
-
     public Long getId() {
         return id;
     }
