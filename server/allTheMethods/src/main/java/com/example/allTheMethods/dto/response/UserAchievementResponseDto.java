8cf3c2acad6bffa7643f39964c2e83408612e773
sotosAdis92 <tsompilis@gmail.com>
2026-07-31 22:47:21 +0300

    added user achievements mapper interface

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/UserAchievementResponseDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/UserAchievementResponseDto.java
@@ -1,6 +1,16 @@
 package com.example.allTheMethods.dto.response;
 
-public record UserAchievementResponseDto(
+import java.time.LocalDateTime;
 
+public record UserAchievementResponseDto(
+        Long id,
+        Long userId,
+        Long achievementId,
+        LocalDateTime achievedAt,
+        String name,
+        String description,
+        String category,
+        String rank,
+        String visibility
 ) {
 }
--- server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/UserAchievementsMapper.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/UserAchievementsMapper.java
@@ -1,6 +1,12 @@
 package com.example.allTheMethods.mapper;
 
 
-public interface UserAchievementsMapper {
+import com.example.allTheMethods.dto.response.UserAchievementResponseDto;
+import com.example.allTheMethods.entity.UserAchievements;
 
+import java.util.List;
+
+public interface UserAchievementsMapper {
+    UserAchievementResponseDto toDto(UserAchievements userAchievements);
+    List<UserAchievementResponseDto> toDto(List<UserAchievements> userAchievements);
 }
