54e781e657396e5f71a6fca86c90820841990a18
sotosAdis92 <tsompilis@gmail.com>
2026-07-31 16:31:17 +0300

    problem mapper interface

--- server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/ProblemMapper.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/mapper/ProblemMapper.java
@@ -1,10 +1,17 @@
 package com.example.allTheMethods.mapper;
 
+import com.example.allTheMethods.dto.request.CreateProblemRequestDto;
+import com.example.allTheMethods.dto.request.UpdateProblemRequestDto;
+import com.example.allTheMethods.dto.response.ProblemResponseDto;
 import com.example.allTheMethods.entity.Problem;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface ProblemMapper {
-    Problem toEntity();
-
+    Problem toEntity(CreateProblemRequestDto createProblemRequestDto);
+    Problem toEntity(UpdateProblemRequestDto updateProblemRequestDto);
+    ProblemResponseDto toDto(Problem problem);
+    List<ProblemResponseDto> toDto(List<Problem> problemList);
 }
