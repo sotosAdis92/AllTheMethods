be6361ac625afdbd168bc9623b259388950754b0
sotosAdis92 <tsompilis@gmail.com>
2026-08-02 23:13:23 +0300

    added alternative to images in the heading file

--- clientside/src/Components/Header.jsx
+++ clientside/src/Components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
     <>
       {isUserLoggedIn ? (
         <div className="container">
-          <img src={img1} alt={""}></img>
+          <img src={img1} alt={img1}></img>
           <h2 className="title">All The Methods</h2>
           <ul className="links">
             <li className="link">
@@ -104,7 +104,7 @@ const Header = () => {
               onClick={handleSignOut}
             >
               Logout
-              <img src={img2} alt={""}></img>
+              <img src={img2} alt={img2}></img>
             </Button>
           </div>
         </div>
