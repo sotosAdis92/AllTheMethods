98cd37da6b8be582a7f1192e2662a2a1ba81cf3e
sotosAdis92 <tsompilis@gmail.com>
2026-08-02 13:18:41 +0300

    added validation to the create problem request dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateProblemRequestDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateProblemRequestDto.java
@@ -4,35 +4,35 @@ import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateProblemRequestDto(
-    @NotBlank(message = "")
+    @NotBlank(message = "Number cannot be blank or null")
     int number,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Title cannot be blank or null")
     String title,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Category cannot be blank or null")
     String category,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Difficulty cannot be blank or null")
     String difficulty,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Description cannot be blank or null")
     String description,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Points cannot be blank or null")
     int points,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Problem String cannot be blank or null")
     String problemString,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Problem Type cannot be blank or null")
     String problemType,
 
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Problem Data cannot be blank or null")
     String problemData,
 
-    @NotBlank(message = "")
+    @NotBlank(message = "Function String cannot be blank or null")
     String functionString
 ) {
 }
