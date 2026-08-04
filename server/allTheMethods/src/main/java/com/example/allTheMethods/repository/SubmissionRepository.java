b0765a0aa64048dcb71a107ae9923ba7882d7119
sotosAdis92 <tsompilis@gmail.com>
2026-08-04 19:45:14 +0300

    added query optimization to the user submissions, returns all submissions

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
@@ -1,10 +1,12 @@
 package com.example.allTheMethods.repository;
 
 import com.example.allTheMethods.entity.Submission;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -13,5 +15,6 @@ import java.util.List;
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
     @Query("SELECT s FROM Submission s JOIN FETCH s.problem p WHERE s.user.id = ?1")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value = "true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     Page<Submission> findAllByUserId(@Param("userId") Long id, Pageable pageable);
 }
