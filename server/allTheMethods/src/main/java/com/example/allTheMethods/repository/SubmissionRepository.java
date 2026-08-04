892a411abe57b2c627b74d4d3894d5dac4d56c0a
sotosAdis92 <tsompilis@gmail.com>
2026-08-05 01:35:15 +0300

    added query hints to the submissions of each user

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/SubmissionRepository.java
@@ -15,6 +15,10 @@ import java.util.List;
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
     @Query("SELECT s FROM Submission s JOIN FETCH s.problem p WHERE s.user.id = ?1")
-    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value = "true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
+    @QueryHints(
+            {
+                    @QueryHint(name="org.hibernate.readOnly",value = "true"),
+                    @QueryHint(name = "org.hibernate.cacheable",value = "true")
+            })
     Page<Submission> findAllByUserId(@Param("userId") Long id, Pageable pageable);
 }
