4b41162cfcab8c95a861e3fb6f11dac5185cf06b
sotosAdis92 <tsompilis@gmail.com>
2026-08-04 19:12:00 +0300

    added query hints for better performance of the query to get the problems

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/ProblemRepository.java
@@ -1,8 +1,10 @@
 package com.example.allTheMethods.repository;
 
 import com.example.allTheMethods.entity.Problem;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -13,6 +15,12 @@ public interface ProblemRepository extends JpaRepository<Problem, Long> {
     List<Problem> findProblemByDifficulty(String difficulty);
 
     @Query("SELECT p FROM Problem p WHERE p.category IN (:categories) AND p.difficulty IN (:difficulties)")
+    @QueryHints({
+            @QueryHint(name = "org.hibernate.readOnly",value = "true"),
+            @QueryHint(name = "org.hibernate.cacheable",value = "true"),
+            @QueryHint(name = "jakarta.persistence.cache.retrieveMode", value = "USE"),
+            @QueryHint(name = "jakarta.persistence.cache.storeMode", value = "USE")
+    })
     List<Problem> findProblemsByCategoryOrDifficulty(@Param("categories") List<String> categories, @Param("difficulties") List<String> difficulties);
 
     List<Problem> findProblemsByCategoryIn(@Param("categories") List<String> categories);
