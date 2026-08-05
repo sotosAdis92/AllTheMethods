706a973cc8932ad9dd85c09baf7f1594e3ebba43
sotosAdis92 <tsompilis@gmail.com>
2026-08-05 03:14:28 +0300

    added query hints to queries in user achievements and user problems to improve performace

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserAchievementsRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserAchievementsRepository.java
@@ -1,8 +1,10 @@
 package com.example.allTheMethods.repository;
 
 import com.example.allTheMethods.entity.UserAchievements;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -10,11 +12,14 @@ import java.util.List;
 @Repository
 public interface UserAchievementsRepository extends JpaRepository<UserAchievements, Long> {
     @Query("SELECT ua from UserAchievements ua JOIN ua.achievement a WHERE ua.user.id = ?1")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     List<UserAchievements> findAllByUserId(Long userId);
 
     @Query("SELECT COUNT(up) FROM UserProblem up WHERE up.user.id = :userId AND up.problem.category = :category")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     Integer countProblemsByCategory(@Param("userId") Long userId, @Param("category") String category);
 
     @Query("SELECT COUNT(ua) > 0 FROM UserAchievements ua WHERE ua.user.id = :userId AND ua.achievement.achievementId = :achievementId")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     boolean existsByUserIdAndAchievementId(Long userId, Long achievementId);
 }
--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
@@ -4,8 +4,10 @@ import com.example.allTheMethods.dto.response.CategoryStatsResponseDto;
 import com.example.allTheMethods.dto.response.DifficultyStatsResponse;
 import com.example.allTheMethods.dto.response.UserProblemStatsResponseDto;
 import com.example.allTheMethods.entity.UserProblem;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -14,15 +16,19 @@ import java.util.List;
 public interface UserProblemsRepository extends JpaRepository<UserProblem, Long> {
 
     @Query("SELECT up FROM UserProblem up JOIN FETCH up.problem p WHERE up.user.id = ?1")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     List<UserProblem> findAllByUserId(Long userId);
 
     @Query("SELECT p.difficulty,COUNT(up) FROM UserProblem up JOIN up.problem p WHERE up.user.id = ?1 GROUP BY p.difficulty")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     List<DifficultyStatsResponse> countAllByUserAndProblemDifficulty(@Param("userId") Long userId);
 
     @Query("SELECT p.difficulty,COUNT(p.difficulty),COUNT(up.user.id) FROM Problem p LEFT JOIN UserProblem up ON p.id = up.problem.id AND up.user.id = ?1 GROUP BY p.difficulty")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     List<UserProblemStatsResponseDto> countDistinctSolvedByDifficultyFromUser(@Param("userId") Long userId);
 
     @Query("SELECT p.problemType,COUNT(p.problemType) FROM Problem p FULL JOIN UserProblem up ON p.id = up.problem.id WHERE up.user.id = ?1 GROUP BY p.problemType")
+    @QueryHints({@QueryHint(name="org.hibernate.readOnly",value="true"),@QueryHint(name = "org.hibernate.cacheable",value = "true")})
     List<CategoryStatsResponseDto> countDistinctByIdAndCategory(@Param("userId") Long userId);
 
     boolean existsByUserIdAndProblemId(Long userId, Long problemId);
