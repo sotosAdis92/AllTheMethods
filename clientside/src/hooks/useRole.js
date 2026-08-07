17c50b91e7617c691b960941f072799029eae3a8
sotosAdis92 <tsompilis@gmail.com>
2026-08-08 01:46:53 +0300

    added use role function to check the role of the user for the ui

--- clientside/src/hooks/useRole.js
+++ clientside/src/hooks/useRole.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import getUser from "../services/UsersService";
+export const ROLES = {};
 export default function useRole() {
   useEffect(() => {
     getUser().then((response) => {
