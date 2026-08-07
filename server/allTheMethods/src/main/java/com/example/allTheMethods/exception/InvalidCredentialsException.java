04c928683a8cd9523307e1bfc9b0d6266815beb1
sotosAdis92 <tsompilis@gmail.com>
2026-08-07 19:30:33 +0300

    added invalid credentials exception

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/exception/InvalidCredentialsException.java
@@ -0,0 +1,10 @@
+package com.example.allTheMethods.exception;
+
+public class InvalidCredentialsException  extends RuntimeException{
+    public InvalidCredentialsException(){
+        super("Invalid username or password");
+    }
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+}
