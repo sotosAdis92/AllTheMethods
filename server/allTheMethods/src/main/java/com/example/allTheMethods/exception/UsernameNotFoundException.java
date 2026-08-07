0f402c3f4985b8ae4dfa7ae05786f68be5cae996
sotosAdis92 <tsompilis@gmail.com>
2026-08-07 19:17:52 +0300

    added custom username not found exception

---
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/exception/UsernameNotFoundException.java
@@ -0,0 +1,5 @@
+package com.example.allTheMethods.exception;
+
+public class UsernameNotFoundException extends RuntimeException{
+    public UsernameNotFoundException(String message) { super(message);}
+}
