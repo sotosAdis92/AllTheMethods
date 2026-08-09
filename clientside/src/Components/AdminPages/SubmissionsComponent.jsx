07a9163683738b2e75d55681a040d1bb7fd80522
sotosAdis92 <tsompilis@gmail.com>
2026-08-09 12:43:55 +0300

    added submission component to the admin pages

--- clientside/src/Components/AdminPages/SubmissionsComponent.jsx
+++ clientside/src/Components/AdminPages/SubmissionsComponent.jsx
@@ -1,2 +1,11 @@
-const SubmissionComponent = () => {};
+import { useState } from "react";
+
+const SubmissionComponent = () => {
+  const [submissions, setSubmissions] = useState([]);
+  const [pageNo, setPageNo] = useState(0);
+  const [pageSize, setPageSize] = useState(30);
+  const [totalPages, setTotalPages] = useState(0);
+  const [count, setCount] = useState(0);
+  const pageNumbers = [];
+};
 export default SubmissionComponent;
