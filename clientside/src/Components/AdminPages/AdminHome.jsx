2e50530c872a109f2128b113d72d93433e2873db
sotosAdis92 <tsompilis@gmail.com>
2026-08-10 22:15:57 +0300

    added loading the components directly in the admin panel instead of having separete screens

--- clientside/src/Components/AdminPages/AdminHome.jsx
+++ clientside/src/Components/AdminPages/AdminHome.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
+import AchievementListComponent from "./AchievementListComponent";
+import ProblemListComponent from "./ProblemListComponent";
+import SubmissionComponent from "./SubmissionsComponent";
+import UsersComponent from "./UsersComponent";
+
 const AdminHome = () => {
+  const [component, setComponent] = useState("problems");
+  const renderComponent = () => {
+    if (component === "problems") {
+      return <ProblemListComponent></ProblemListComponent>;
+    } else if (component === "achievement") {
+      return <AchievementListComponent></AchievementListComponent>;
+    } else if (component === "submission") {
+      return <SubmissionComponent></SubmissionComponent>;
+    } else if (component === "users") {
+      return <UsersComponent></UsersComponent>;
+    }
+  };
   return (
-    <div className="containerAdmin">
-      <div></div>
-    </div>
+    <>
+      <div className="containerAdmin">
+        <div>
+          <button onClick={() => setComponent("problems")}>Problems</button>
+        </div>
+        <div>
+          <button onClick={() => setComponent("achievement")}>
+            Achievements
+          </button>
+        </div>
+        <div>
+          <button onClick={() => setComponent("submission")}>
+            Submissions
+          </button>
+        </div>
+        <div>
+          <button onClick={() => setComponent("users")}>Users</button>
+        </div>
+      </div>
+      <div>{renderComponent()}</div>
+    </>
   );
 };
 export default AdminHome;
