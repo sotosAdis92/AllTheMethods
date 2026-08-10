672520d4db210a40529dbae1cdb39f5f613468e4
sotosAdis92 <tsompilis@gmail.com>
2026-08-10 16:23:36 +0300

    added css styling to the achievement component

--- clientside/src/Components/AdminPages/AchievementListComponent.jsx
+++ clientside/src/Components/AdminPages/AchievementListComponent.jsx
@@ -1,2 +1,29 @@
-const AchievementListComponent = () => {};
+import { useEffect, useState } from "react";
+import { listAchievements } from "../../services/AchievementService";
+import "./achievementListComponent.css";
+const AchievementListComponent = () => {
+  const [achievements, setAchievements] = useState([]);
+  useEffect(() => {
+    listAchievements().then((response) => {
+      console.log(response.data);
+      setAchievements(response.data);
+    });
+  }, []);
+
+  const listOfAchievements = achievements.map((achievement) => (
+    <div key={achievement.id} className="achievement-item-table">
+      <div>{achievement.id}</div>
+      <div>{achievement.name}</div>
+      <div>{achievement.description}</div>
+      <div>{achievement.category}</div>
+      <div>{achievement.rank}</div>
+    </div>
+  ));
+
+  return (
+    <div>
+      <div>{listOfAchievements}</div>
+    </div>
+  );
+};
 export default AchievementListComponent;
