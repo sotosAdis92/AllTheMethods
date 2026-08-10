e0ad638bb6a624cbd50ec3e6e0ea0b5ff9b715b2
sotosAdis92 <tsompilis@gmail.com>
2026-08-11 00:48:47 +0300

    added pagination to the admin submissions page

--- clientside/src/Components/AdminPages/SubmissionsComponent.jsx
+++ clientside/src/Components/AdminPages/SubmissionsComponent.jsx
@@ -1,18 +1,24 @@
 import { useEffect, useState } from "react";
+import arrowback from "../../assets/arrowback_2_10.png";
+import arrowfront from "../../assets/arrowfront_10.png";
 import { getAllSubmissions } from "../../services/SubmitService";
 import "./SubmissionComponent.css";
 const SubmissionComponent = () => {
   const [submissions, setSubmissions] = useState([]);
-  const [pageNo, setPageNo] = useState(0);
-  const [pageSize, setPageSize] = useState(30);
-  const [totalPages, setTotalPages] = useState(0);
   const [count, setCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(30);
+  const [sortBy, setSortBy] = useState("");
+  const [sortDir, setSortDir] = useState("DESC");
+  const [totalPages, setTotalPages] = useState(0);
   const pageNumbers = [];
 
   useEffect(() => {
     getAllSubmissions().then((response) => {
-      console.log(response.data.content);
+      console.log(response.data);
       setSubmissions(response.data.content);
+      setCount(response.data.numberOfElements);
+      setTotalPages(response.data.totalPages);
     });
   }, []);
 
@@ -26,9 +32,44 @@ const SubmissionComponent = () => {
     </div>
   ));
 
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const pageNumberButtons = pageNumbers.map((number) => (
+    <button
+      key={number}
+      onClick={() => setPageNumber(number)}
+      className={`buttonNumberPage ${pageNumber === number ? "active" : ""}`}
+    >
+      {number}
+    </button>
+  ));
+
   return (
     <div>
       <div>{listOfSubmissions}</div>
+      {totalPages > 1 ? (
+        <div className="pageButtonsContainer">
+          <button
+            className="pageButton"
+            onClick={() => setPageNumber((page) => Math.max(page - 1, 0))}
+            disabled={pageNumber === 1}
+          >
+            <img src={arrowback} alt={arrowback}></img>
+          </button>
+          {pageNumberButtons}
+          <button
+            className="pageButton"
+            onClick={() => setPageNumber((page) => Math.max(page, totalPages))}
+            disabled={pageNumber === totalPages}
+          >
+            <img src={arrowfront} alt={arrowfront}></img>
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
