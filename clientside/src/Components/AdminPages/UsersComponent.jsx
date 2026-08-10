b4c121a211749f51586b6a6fa0545889400768e1
sotosAdis92 <tsompilis@gmail.com>
2026-08-11 00:50:07 +0300

    added pagination to the user admin panel

--- clientside/src/Components/AdminPages/UsersComponent.jsx
+++ clientside/src/Components/AdminPages/UsersComponent.jsx
@@ -1,26 +1,46 @@
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
+import arrowback from "../../assets/arrowback_2_10.png";
+import arrowfront from "../../assets/arrowfront_10.png";
 import { deleteUserById, getAllUsers } from "../../services/UsersService";
 import "./UsersComponent.css";
 const UsersComponent = () => {
   const [users, setUsers] = useState([]);
-  const [pageNo, setPageNo] = useState(0);
-  const [pageSize, setPageSize] = useState(30);
-  const [totalPages, setTotalPages] = useState(0);
   const [count, setCount] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [pageSize, setPageSize] = useState(30);
+  const [sortBy, setSortBy] = useState("");
+  const [sortDir, setSortDir] = useState("DESC");
+  const [totalPages, setTotalPages] = useState(0);
   const pageNumbers = [];
 
   useEffect(() => {
     getAllUsers().then((response) => {
       console.log(response.data.content);
       setUsers(response.data.content);
+      setCount(response.data.numberOfElements);
+      setTotalPages(response.data.totalPages);
     });
   }, []);
 
   const deleteUser = (userId) => {
     deleteUserById(userId);
   };
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const pageNumberButtons = pageNumbers.map((number) => (
+    <button
+      key={number}
+      onClick={() => setPageNumber(number)}
+      className={`buttonNumberPage ${pageNumber === number ? "active" : ""}`}
+    >
+      {number}
+    </button>
+  ));
 
   const listOfUsers = users.map((user) => (
     <div className="user-item-table" key={user.id}>
@@ -37,6 +57,27 @@ const UsersComponent = () => {
   return (
     <div>
       <div>{listOfUsers}</div>
+      {totalPages > 1 ? (
+        <div className="pageButtonsContainer">
+          <button
+            className="pageButton"
+            onClick={() => setPageNumber((page) => Math.max(page - 1, 0))}
+            disabled={pageNumber === 1}
+          >
+            <img src={arrowback} alt={arrowback}></img>
+          </button>
+          {pageNumberButtons}
+          <button
+            className="pageButton"
+            onClick={() => setPageNumber((page) => Math.max(page, totalPages))}
+            disabled={pageNumber === totalPages}
+          >
+            <img src={arrowfront} alt={arrowfront}></img>
+          </button>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
