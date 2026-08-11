a49db20b155f4b06325620500947e4093f02b263
sotosAdis92 <tsompilis@gmail.com>
2026-08-11 19:29:58 +0300

    added validation to the problem creation dto

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateProblemRequestDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateProblemRequestDto.java
@@ -1,37 +1,61 @@
 package com.example.allTheMethods.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.Length;
 
 public record CreateProblemRequestDto(
-    @NotBlank(message = "Number cannot be blank or null")
+    @NotBlank(message = "Number cannot be blank")
+    @NotEmpty(message = "Number cannot be empty")
+    @NotNull(message = "Number cannot be null")
+    @Positive(message = "Number must be a positive")
     int number,
 
-    @NotBlank(message = "Title cannot be blank or null")
+    @NotEmpty(message = "Title cannot be empty")
+    @NotNull(message = "Title cannot be null")
+    @NotBlank(message = "Title cannot be blank")
     String title,
 
+    @NotEmpty(message = "Category cannot be empty")
+    @NotNull(message = "Category cannot be null")
     @NotBlank(message = "Category cannot be blank or null")
     String category,
 
+    @NotEmpty(message = "Difficulty cannot be empty")
+    @NotNull(message = "Difficulty cannot be null")
     @NotBlank(message = "Difficulty cannot be blank or null")
     String difficulty,
 
+    @NotEmpty(message = "Description cannot be empty")
+    @NotNull(message = "Description cannot be null")
     @NotBlank(message = "Description cannot be blank or null")
     String description,
 
+    @NotEmpty(message = "Points cannot be empty")
+    @NotNull(message = "Points cannot be null")
     @NotBlank(message = "Points cannot be blank or null")
+    @Positive(message = "Points must be a positive")
     int points,
 
+    @NotEmpty(message = "Problem String cannot be empty")
+    @NotNull(message = "Problem String cannot be null")
     @NotBlank(message = "Problem String cannot be blank or null")
     String problemString,
 
+    @NotEmpty(message = "Problem Type cannot be empty")
+    @NotNull(message = "Problem Type cannot be null")
     @NotBlank(message = "Problem Type cannot be blank or null")
     String problemType,
 
-
+    @NotEmpty(message = "Problem Data cannot be empty")
+    @NotNull(message = "Problem Data cannot be null")
     @NotBlank(message = "Problem Data cannot be blank or null")
     String problemData,
 
+    @NotEmpty(message = "Function String cannot be empty")
+    @NotNull(message = "Function String cannot be null")
     @NotBlank(message = "Function String cannot be blank or null")
     String functionString
 ) {
--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateSubmissionRequestDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateSubmissionRequestDto.java
@@ -1,15 +1,23 @@
 package com.example.allTheMethods.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateSubmissionRequestDto(
+        @NotBlank(message = "Number cannot be blank")
+        @NotEmpty(message = "Number cannot be empty")
         @NotNull(message = "ID cannot be null")
         Long userId,
 
+        @NotBlank(message = "Number cannot be blank")
+        @NotEmpty(message = "Number cannot be empty")
         @NotNull(message = "Problem ID cannot be null")
         Long problemId,
 
+        @NotBlank(message = "Number cannot be blank")
+        @NotEmpty(message = "Number cannot be empty")
         @NotNull(message = "Date cannot be null")
         String submittedAt
 ) {
