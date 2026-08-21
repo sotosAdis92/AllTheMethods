8aa6a368532233f057bcb03a548fbd5fe101f9ea
sotosAdis92 <tsompilis@gmail.com>
2026-08-22 00:23:54 +0300

    added user count problems total and acceptance rate to the summary component

--- clientside/src/Components/UserComponents/UserSummaryReport.jsx
+++ clientside/src/Components/UserComponents/UserSummaryReport.jsx
@@ -1,6 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getUserProblemSummaryReport } from "../../services/UserProblemService";
 const UserSummaryReport = (props) => {
   const [countProblems, setCountProblems] = useState([]);
   const [acceptanceRate, setAcceptanceRate] = useState([]);
+  const userId = props.userId;
+
+  useEffect(() => {
+    getUserProblemSummaryReport(userId).then((response) => {
+      setCountProblems(response.data.countTotalProblems);
+      setAcceptanceRate(response.data.userAcceptanceRate);
+    });
+  });
+
+  return (
+    <>
+      <div>
+        <div>{countProblems}</div>
+        <div>{acceptanceRate}</div>
+      </div>
+    </>
+  );
 };
 export default UserSummaryReport;
