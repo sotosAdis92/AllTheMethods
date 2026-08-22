36119e8e0e56e4e1129928dd1dfd50a919e5237f
sotosAdis92 <tsompilis@gmail.com>
2026-08-22 15:02:52 +0300

    added feature: added submissions counter to the summary of the user

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/SummaryResponseDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/response/SummaryResponseDto.java
@@ -2,6 +2,7 @@ package com.example.allTheMethods.dto.response;
 
 public record SummaryResponseDto(
         Long countTotalProblems,
+        Long countSubmissions,
         Double userAcceptanceRate
 ) {
 }
