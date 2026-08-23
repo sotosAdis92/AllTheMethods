c088fe130d1fb3ecbb946dfb171669046bce585e
sotosAdis92 <tsompilis@gmail.com>
2026-08-23 20:06:00 +0300

    added new sql case query for the edge case of the count of the problems being zero, so that in the acceptance rate calculation, the division dosent happend by zero

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
@@ -38,7 +38,7 @@ public interface UserProblemsRepository extends JpaRepository<UserProblem, Long>
 
     boolean existsByUserIdAndProblemId(Long userId, Long problemId);
 
-    @Query("SELECT COUNT(DISTINCT up.problem.id),COUNT(s),100.0*COUNT(DISTINCT up.user.id) / COUNT(s.user.id) FROM Submission s JOIN UserProblem up ON s.user.id = up.user.id WHERE up.user.id = ?1")
+    @Query("SELECT COUNT(DISTINCT up.problem.id),COUNT(s),CASE WHEN COUNT(s.user.id) = 0 THEN 0 ELSE 100.0*COUNT(DISTINCT up.user.id) / COUNT(s.user.id) END FROM Submission s JOIN UserProblem up ON s.user.id = up.user.id WHERE up.user.id = ?1")
     @QueryHints({@QueryHint(name = "org.hibernate.readOnly", value = "true"), @QueryHint( name= "org.hibernate.cacheable",value = "true")})
     List<SummaryResponseDto> countSummeryOfUser(@Param("userId") Long userId);
 }
