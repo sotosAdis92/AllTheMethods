1d9eea851f40c30e13af71e533e279841e9b0612
sotosAdis92 <tsompilis@gmail.com>
2026-08-26 19:18:36 +0300

    minor changes to the application properties

--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/UserController.java
@@ -45,6 +45,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("#id == authentication.principal.id")
     public ResponseEntity<String> deleteUser(@PathVariable int id){
         usersService.deleteUser(id);
         return ResponseEntity.ok("Deleted user");
