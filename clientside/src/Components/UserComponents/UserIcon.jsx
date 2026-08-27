bf09de4682c85351514a504ea52713b5ef0aab4b
sotosAdis92 <tsompilis@gmail.com>
2026-08-27 13:19:41 +0300

    Css styling added to the edit profile button

--- clientside/src/Components/UserComponents/UserIcon.jsx
+++ clientside/src/Components/UserComponents/UserIcon.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import img1 from "../../assets/userPicDefault.png";
+import "./UserIcon.css";
+
 const UserIcon = (props) => {
   const navigator = useNavigate();
   const navigateToEdit = () => {
