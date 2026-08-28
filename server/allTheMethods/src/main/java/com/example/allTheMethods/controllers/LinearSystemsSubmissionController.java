b4f8aaa2e9f9e93bda3e950583559a663ba93c90
sotosAdis92 <tsompilis@gmail.com>
2026-08-28 18:28:27 +0300

    Added gershgorin circles algorithm api endpoint

--- server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/LinearSystemsSubmissionController.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/controllers/LinearSystemsSubmissionController.java
@@ -1,6 +1,9 @@
 package com.example.allTheMethods.controllers;
 
+import com.example.allTheMethods.dto.request.LinearSystemsDataDto;
 import com.example.allTheMethods.service.SubmissionServiceLinearSystems;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
@@ -10,6 +13,11 @@ public class LinearSystemsSubmissionController {
     private SubmissionServiceLinearSystems submissionServiceLinearSystems;
     public LinearSystemsSubmissionController(SubmissionServiceLinearSystems submissionServiceLinearSystems){
         this.submissionServiceLinearSystems = submissionServiceLinearSystems;
+    }
+
+    @PostMapping("/gershgorin")
+    public ResponseEntity<?> gershgorinCirclesAlgorithm(LinearSystemsDataDto linearSystemsDataDto){
+        return new ResponseEntity<>(submissionServiceLinearSystems.gershgorinCirclesAlgorithm(linearSystemsDataDto), HttpStatus.OK);
     }
 
 }
