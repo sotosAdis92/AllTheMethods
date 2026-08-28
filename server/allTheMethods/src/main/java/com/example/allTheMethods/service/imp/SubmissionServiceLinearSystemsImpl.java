f9cbecc0183acc2fd315647f41ae7ccb982cea26
sotosAdis92 <tsompilis@gmail.com>
2026-08-28 14:00:55 +0300

    slight optimization made to the gershgorin circles algorithm

--- server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceLinearSystemsImpl.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/service/imp/SubmissionServiceLinearSystemsImpl.java
@@ -39,14 +39,8 @@ public class SubmissionServiceLinearSystemsImpl implements SubmissionServiceLine
         }
 
         for(i=0;i<matrix.length;i++){
-            if(rowSums.get(i) < colSums.get(i)){
-                mins.add(rowSums.get(i));
-            }
-            else{
-                mins.add(colSums.get(i));
-            }
+            mins.add(Math.min(rowSums.get(i),colSums.get(i)));
         }
-
         for(i=0;i<3;i++){
             System.out.println(mins.get(i));
         }
