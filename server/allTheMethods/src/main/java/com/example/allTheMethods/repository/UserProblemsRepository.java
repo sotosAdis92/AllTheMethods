0f0712253f287563b784c83333313a09ab63d15c
sotosAdis92 <tsompilis@gmail.com>
2026-08-31 02:39:41 +0300

    Fixed bug when null handling in summary query

--- server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/repository/UserProblemsRepository.java
@@ -38,7 +38,7 @@ public interface UserProblemsRepository extends JpaRepository<UserProblem, Long>
 
     boolean existsByUserIdAndProblemId(Long userId, Long problemId);
 
-    @Query("SELECT COALESCE(COUNT(DISTINCT s.problem.id), 0),COALESCE(COUNT(s), 0),COALESCE(CASE WHEN COUNT(s) = 0 THEN 0.0 ELSE 100.0*COUNT(DISTINCT up.user.id) / COUNT(s) END,0.0) FROM Submission s JOIN UserProblem up ON s.user.id = up.user.id WHERE up.user.id = ?1")
+    @Query("SELECT COALESCE(COUNT(DISTINCT up.problem.id), 0),COALESCE(COUNT(s), 0),COALESCE(CASE WHEN COUNT(s) = 0 OR COUNT(DISTINCT up.problem.id) = 0 THEN 0.0 ELSE 100.0 * COUNT(DISTINCT up.problem.id) / COUNT(s) END, 0.0) FROM Submission s LEFT JOIN UserProblem up ON s.user.id = up.user.id AND s.problem.id = up.problem.id WHERE s.user.id = ?1")
     @QueryHints({@QueryHint(name = "org.hibernate.readOnly", value = "true"), @QueryHint( name= "org.hibernate.cacheable",value = "true")})
     SummaryResponseDto countSummeryOfUser(@Param("userId") Long userId);
 }
