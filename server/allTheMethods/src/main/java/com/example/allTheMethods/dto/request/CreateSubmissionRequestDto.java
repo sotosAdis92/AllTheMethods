468049db914d7ed1bc1949820f274b510eb04198
sotosAdis92 <tsompilis@gmail.com>
2026-08-30 22:26:16 +0300

    removed not empty and not blank from validation for long fields

--- server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateSubmissionRequestDto.java
+++ server/allTheMethods/src/main/java/com/example/allTheMethods/dto/request/CreateSubmissionRequestDto.java
@@ -1,23 +1,15 @@
 package com.example.allTheMethods.dto.request;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateSubmissionRequestDto(
-        @NotBlank(message = "ID cannot be blank")
-        @NotEmpty(message = "ID cannot be empty")
         @NotNull(message = "ID cannot be null")
         Long userId,
 
-        @NotBlank(message = "Problem ID cannot be blank")
-        @NotEmpty(message = "Problem ID cannot be empty")
         @NotNull(message = "Problem ID cannot be null")
         Long problemId,
 
-        @NotBlank(message = "Date cannot be blank")
-        @NotEmpty(message = "Date cannot be empty")
         @NotNull(message = "Date cannot be null")
         String submittedAt
 ) {
