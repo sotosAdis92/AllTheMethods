2a5f4460f767876bea634257235e8b98720a75f6
sotosAdis92 <tsompilis@gmail.com>
2026-08-30 17:21:45 +0300

    Testing out generation of gershgorin inputs

--- clientside/src/hooks/useGenerateGershgorinInputs.js
+++ clientside/src/hooks/useGenerateGershgorinInputs.js
@@ -1,7 +1,32 @@
 export default function useGenerateInputsGershgorin(matrix, entries) {
-  for (let i = 0; i < 2 * matrix.length(); i++) {
+  const matrixSize = matrix.length();
+  for (let i = 0; i < matrixSize; i++) {
     entries.push({
-      id: i,
+      iid: i * 2,
+      placeholder: `min P${i + 1}`,
+      type: "text",
+      label: i === 0 ? "P1 = " : "",
+      errorMessage: "Input should be a floating point/double number",
+      name: `P${i + 1}_min`,
+      required: true,
+      pattern: /^[+-]?\d+(\.\d+)?$/,
+      position: "left",
+      index: i,
+      pairId: i,
+    });
+
+    entries.push({
+      id: i * 2 + 1,
+      placeholder: `max P${i + 1}`,
+      type: "text",
+      label: ` < P${i + 1} < `,
+      errorMessage: "Input should be a floating point/double number",
+      name: `P${i + 1}_max`,
+      required: true,
+      pattern: /^[+-]?\d+(\.\d+)?$/,
+      position: "right",
+      index: i,
+      pairId: i,
     });
   }
 }
