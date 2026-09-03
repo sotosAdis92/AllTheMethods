83022b04ee8859ca10ecbfdc7659f9bc93728a8e
sotosAdis92 <tsompilis@gmail.com>
2026-09-03 14:30:14 +0300

    Fixing data fetching in the problems list to make the ui load faster

--- clientside/src/main.jsx
+++ clientside/src/main.jsx
@@ -1,12 +1,11 @@
 import { SnackbarProvider } from "notistack";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <SnackbarProvider maxSnack={3}>
-      <App />
-    </SnackbarProvider>
-  </StrictMode>,
+  //<StrictMode>
+  <SnackbarProvider maxSnack={3}>
+    <App />
+  </SnackbarProvider>,
+  //</StrictMode>,
 );
