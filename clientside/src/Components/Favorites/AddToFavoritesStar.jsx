f10fd68a5c57ac36304bffdbd64506df6ea3ced4
sotosAdis92 <tsompilis@gmail.com>
2026-09-04 17:55:49 +0300

    Minor ui changes

--- clientside/src/Components/Favorites/AddToFavoritesStar.jsx
+++ clientside/src/Components/Favorites/AddToFavoritesStar.jsx
@@ -20,7 +20,7 @@ const AddToFavouritesStar = () => {
         onClick={() => handleFavorite()}
         onAnimationEnd={() => setAnimate(false)}
       >
-        <svg width="28" height="28" viewBox="0 0 24 24" className="star">
+        <svg width="22" height="22" viewBox="0 0 24 24" className="star">
           <path
             d="M12 2L14.8 8.2L21.5 9.1L16.8 13.6L17.9 20.3L12 17.1L6.1 20.3L7.2 13.6L2.5 9.1L9.2 8.2L12 2Z"
             strokeLinejoin="round"
