c98ae5c90c311e30dbaf64defd7c372a3e2a8afe
sotosAdis92 <tsompilis@gmail.com>
2026-09-14 00:08:29 +0300

    Added css classes to the problem type component

--- clientside/src/Components/ProblemTypes/ProblemTypesComponent.jsx
+++ clientside/src/Components/ProblemTypes/ProblemTypesComponent.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import "./ProblemTypesComponent.css";
 const ProblemCategoriesComponent = ({ problemTypes }) => {
+  const navigator = useNavigate();
   return (
     <div className="problemTypeContainer">
       <div className="problemType">
-        {problemTypes.map((problemTypes) => (
-          <div>{problemTypes}</div>
+        {problemTypes.map((problemType) => (
+          <div className="type" onClick={() => navigator(`/${problemType}`)}>
+            {problemType}
+          </div>
         ))}
       </div>
     </div>
