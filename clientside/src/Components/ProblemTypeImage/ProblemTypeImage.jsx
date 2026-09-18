7c9157438ab6dbbba9e6ff049cf881ccb830b505
sotosAdis92 <tsompilis@gmail.com>
2026-09-18 20:47:01 +0300

    Added gershgorin circles image

--- clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
+++ clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
@@ -1,8 +1,10 @@
 import bisection from "../../assets/bisection.png";
-
+import circles from "../../assets/circles.png";
 const ProblemTypeImage = (props) => {
   if (props.type === "Bisection") {
     return <img src={bisection} className="imageType"></img>;
+  } else if (props.type === "Gershgorin-Circles") {
+    return <img src={circles} className="imageType"></img>;
   }
 };
 export default ProblemTypeImage;
