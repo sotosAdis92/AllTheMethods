0dc50bca6ffb358ee8663447bc02aaf083296487
sotosAdis92 <tsompilis@gmail.com>
2026-09-19 14:46:34 +0300

    Added pictures for simpson and five point derivative methods

--- clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
+++ clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
@@ -1,6 +1,8 @@
 import bisection from "../../assets/bisection.png";
 import circles from "../../assets/circles.png";
 import deuler from "../../assets/deuler.png";
+import fivepoint from "../../assets/fivepoint.png";
+import simpson from "../../assets/simpson.png";
 import trapezodial from "../../assets/trapezodial.png";
 const ProblemTypeImage = (props) => {
   if (props.type === "Bisection") {
@@ -8,9 +10,13 @@ const ProblemTypeImage = (props) => {
   } else if (props.type === "Gershgorin-Circles") {
     return <img src={circles} className="imageType"></img>;
   } else if (props.type === "Trapezodial-Rule") {
-    return <img src={trapezodial}></img>;
+    return <img src={trapezodial} className="imageType"></img>;
   } else if (props.type === "Direct-Euler") {
-    return <img src={deuler}></img>;
+    return <img src={deuler} className="imageType"></img>;
+  } else if (props.type === "Simpson") {
+    return <img src={simpson} className="imageType"></img>;
+  } else if (props.type === "Five-Point-Derivative") {
+    return <img src={fivepoint} className="imageType"></img>;
   }
 };
 export default ProblemTypeImage;
