3fa75909474e503c8380b9e3df57a892cacc5aeb
sotosAdis92 <tsompilis@gmail.com>
2026-09-22 19:41:18 +0300

    Added foundation for the toggle switch for dark and light modes

---
+++ clientside/src/Components/ToggleTheme/ToggleTheme.jsx
@@ -0,0 +1,12 @@
+const ToggleTheme = () => {
+  return (
+    <div>
+      <input type="checkbox" id="themeswitchcheckbox">
+        <label for="themeswitchcheckbox" className="label">
+          <div className="ball"></div>
+        </label>
+      </input>
+    </div>
+  );
+};
+export default ToggleTheme;
