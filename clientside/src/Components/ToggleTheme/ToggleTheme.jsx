fe67b73e288f6630c351c5cbf2ad5119905a44aa
sotosAdis92 <tsompilis@gmail.com>
2026-09-22 21:48:04 +0300

    Added foundation for the toggle switch for dark and light modes

--- clientside/src/Components/ToggleTheme/ToggleTheme.jsx
+++ clientside/src/Components/ToggleTheme/ToggleTheme.jsx
@@ -1,8 +1,12 @@
+import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const ToggleTheme = () => {
   return (
     <div>
       <input type="checkbox" id="themeswitchcheckbox">
         <label for="themeswitchcheckbox" className="label">
+          <FontAwesomeIcon icon={faSun}></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faMoon}></FontAwesomeIcon>
           <div className="ball"></div>
         </label>
       </input>
