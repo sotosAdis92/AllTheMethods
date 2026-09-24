ce12f8791a3fc31b9820e66d0d6ac5965f5c0260
sotosAdis92 <tsompilis@gmail.com>
2026-09-24 22:44:43 +0300

    Added images for improved euler, fixed point and regula falsi

--- clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
+++ clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
@@ -1,7 +1,10 @@
 import bisection from "../../assets/bisection.png";
 import circles from "../../assets/circles.png";
 import deuler from "../../assets/deuler.png";
+import falsi from "../../assets/falsi.png";
 import fivepoint from "../../assets/fivepoint.png";
+import fixed from "../../assets/fixed.png";
+import ieuler from "../../assets/ieuler.png";
 import newtonraphson from "../../assets/newtonraphson.png";
 import richardson from "../../assets/richardson.png";
 import simpson from "../../assets/simpson.png";
@@ -29,13 +32,13 @@ const ProblemTypeImage = (props) => {
   } else if (props.type === "Diakriti-Newton-Raphson") {
     return <img src={richardson} className="imageType"></img>;
   } else if (props.type === "Regula-Falsi") {
-    return <img src={richardson} className="imageType"></img>;
+    return <img src={falsi} className="imageType"></img>;
   } else if (props.type === "Improved-Euler") {
-    return <img src={richardson} className="imageType"></img>;
+    return <img src={ieuler} className="imageType"></img>;
   } else if (props.type === "Runge-Kutta") {
     return <img src={richardson} className="imageType"></img>;
   } else if (props.type === "Fixed-Point") {
-    return <img src={richardson} className="imageType"></img>;
+    return <img src={fixed} className="imageType"></img>;
   }
 };
 export default ProblemTypeImage;
