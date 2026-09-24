e443e3e6e9587664b2ab900362c07533ae547b55
sotosAdis92 <tsompilis@gmail.com>
2026-09-25 00:27:08 +0300

    Added image for the runge kutta and discrete newton raphson methods

--- clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
+++ clientside/src/Components/ProblemTypeImage/ProblemTypeImage.jsx
@@ -1,12 +1,14 @@
 import bisection from "../../assets/bisection.png";
 import circles from "../../assets/circles.png";
 import deuler from "../../assets/deuler.png";
+import discretenewtonraphson from "../../assets/dnr.png";
 import falsi from "../../assets/falsi.png";
 import fivepoint from "../../assets/fivepoint.png";
 import fixed from "../../assets/fixed.png";
 import ieuler from "../../assets/ieuler.png";
 import newtonraphson from "../../assets/newtonraphson.png";
 import richardson from "../../assets/richardson.png";
+import rungekutta from "../../assets/runge.png";
 import simpson from "../../assets/simpson.png";
 import threepoint from "../../assets/threepoint.png";
 import trapezodial from "../../assets/trapezodial.png";
@@ -30,13 +32,13 @@ const ProblemTypeImage = (props) => {
   } else if (props.type === "Richardson") {
     return <img src={richardson} className="imageType"></img>;
   } else if (props.type === "Diakriti-Newton-Raphson") {
-    return <img src={richardson} className="imageType"></img>;
+    return <img src={discretenewtonraphson} className="imageType"></img>;
   } else if (props.type === "Regula-Falsi") {
     return <img src={falsi} className="imageType"></img>;
   } else if (props.type === "Improved-Euler") {
     return <img src={ieuler} className="imageType"></img>;
   } else if (props.type === "Runge-Kutta") {
-    return <img src={richardson} className="imageType"></img>;
+    return <img src={rungekutta} className="imageType"></img>;
   } else if (props.type === "Fixed-Point") {
     return <img src={fixed} className="imageType"></img>;
   }
